674f1683edbc38f6231852b9f02043458b322703
alex <alex@egforyou.com>
2018-06-23 17:15:06 +0800

    新增测试付款工具 收付宝代付

---
+++ common_pay/test/mysql.user.php
@@ -0,0 +1,19 @@
+<?php
+$db_user_utf8 = "taiwan";
+$db_pwd_utf8 = "xbXqATAWsiXSFlWYGZIe";
+
+$db_user_gbk = "taiwan";
+$db_pwd_gbk = "xbXqATAWsiXSFlWYGZIe";
+?><?php
+$db_user_utf8 = "taiwan";
+$db_pwd_utf8 = "xbXqATAWsiXSFlWYGZIe";
+
+$db_user_gbk = "taiwan";
+$db_pwd_gbk = "xbXqATAWsiXSFlWYGZIe";
+?><?php
+$db_user_utf8 = "taiwan";
+$db_pwd_utf8 = "xbXqATAWsiXSFlWYGZIe";
+
+$db_user_gbk = "taiwan";
+$db_pwd_gbk = "xbXqATAWsiXSFlWYGZIe";
+?>
