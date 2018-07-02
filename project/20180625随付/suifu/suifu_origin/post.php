35fb62216e3fa3ff51b708231e4c4a582aab1815
alex <alex@egforyou.com>
2018-07-02 13:48:48 +0800

    修改SQL

--- project/20180625随付/suifu/suifu_origin/post.php
+++ project/20180625随付/suifu/suifu_origin/post.php
@@ -1,6 +1,6 @@
 <?php
-// include_once("../../../database/mysql.config.php");
-include_once("../../../database/mysql.php");
+include_once("../../../database/mysql.config.php");
+// include_once("../../../database/mysql.php");
 include_once("../moneyfunc.php");
 
 #function
