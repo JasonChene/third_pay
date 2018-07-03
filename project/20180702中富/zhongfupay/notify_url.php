7b3515dfed7d14ae17a1abb75d8e6ca77ba263b4
xsecret <xsecret@egforyou.com>
2018-07-03 16:42:10 +0800

    aaa

--- project/20180702中富/zhongfupay/notify_url.php
+++ project/20180702中富/zhongfupay/notify_url.php
@@ -63,33 +63,33 @@ if ($success_msg == $success_code) {
 		$result_insert = update_online_money($order_no, $mymoney);
 		if ($result_insert == -1) {
 			echo ("会员信息不存在，无法入账");
-			write_log("会员信息不存在，无法入账");
+			//write_log("会员信息不存在，无法入账");
 			exit;
 		}else if($result_insert == 0){
 			echo ($echo_msg);
-			write_log($echo_msg.'at 0');
+			//write_log($echo_msg.'at 0');
 			exit;
 		}else if($result_insert == -2){
 			echo ("数据库操作失败");
-			write_log("数据库操作失败");
+			//write_log("数据库操作失败");
 			exit;
 		}else if($result_insert == 1){
 			echo ($echo_msg);
-			write_log($echo_msg.'at 1');
+			//write_log($echo_msg.'at 1');
 			exit;
 		} else {
 			echo ("支付失败");
-			write_log("支付失败");
+			//write_log("支付失败");
 			exit;
 		}
 	}else{
 		echo ('签名不正确！');
-		write_log("签名不正确！");
+		//write_log("签名不正确！");
 		exit;
 	}
 }else{
 	echo ("交易失败");
-	write_log("交易失败");
+	//write_log("交易失败");
 	exit;
 }
 
