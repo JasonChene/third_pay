a7599695d94cc689c0f7935f560c7aa27deb0464
alex.chang <alex.chang@egforyou.com>
2018-07-04 16:35:37 +0800

    新增第三方
    20180703万象支付

---
+++ project/20180703万象支付/wanxiang/post.php
@@ -0,0 +1,105 @@
+<?php
+header("Content-type:text/html; charset=utf-8");
+include_once("../../../database/mysql.php");//现数据库的连接方式
+include_once("../moneyfunc.php");
+#预设时间在上海
+date_default_timezone_set('PRC');
+if (function_exists("date_default_timezone_set")) {
+  date_default_timezone_set("Asia/Shanghai");
+}
+
+#获取第三方资料(非必要不更动)
+$pay_type = $_REQUEST['pay_type'];
+$params = array(':pay_type' => $pay_type);
+$sql = "select t.pay_name,t.mer_id,t.mer_key,t.mer_account,t.pay_type,t.pay_domain,t1.wy_returnUrl,t1.wx_returnUrl,t1.zfb_returnUrl,t1.wy_synUrl,t1.wx_synUrl,t1.zfb_synUrl from pay_set t left join pay_list t1 on t1.pay_name=t.pay_name where t.pay_type=:pay_type";
+$stmt = $mysqlLink->sqlLink("write1")->prepare($sql);//现数据库的连接方式
+$stmt->execute($params);
+$row = $stmt->fetch();
+$pay_mid = $row['mer_id'];//商户号
+$pay_mkey = $row['mer_key'];//商戶私钥
+$pay_account = $row['mer_account'];
+$return_url = $row['pay_domain'] . $row['wx_returnUrl'];//return跳转地址
+$merchant_url = $row['pay_domain'] . $row['wx_synUrl'];//notify回传地址
+if ($pay_mid == "" || $pay_mkey == "") {
+  echo "非法提交参数";
+  exit;
+}
+#固定参数设置
+$top_uid = $_REQUEST['top_uid'];
+$order_no = getOrderNo();
+$mymoney = number_format($_REQUEST['MOAmount'], 2, '.', '');
+
+#第三方参数设置
+$data = array(
+  "tenantId" => $pay_mid, //商户号
+  "tenantOrderNo" => $order_no, //商户订单号
+  "amount" => number_format($_REQUEST['MOAmount'], 2, '.', ''), //金额 100<=100的倍数<=3000
+  "payWaySan" => '', //支付方式
+  "goodInfo" => 'goodInfo', //商品信息
+  "callBackUrl" => str_replace("http://", null, $merchant_url), //异步商户
+  "callBackViewUrl" => $return_url, //同步商户
+  "schlep" => '', //附加参数
+  "sign_" => '', //下单签名
+);
+
+#变更参数设置
+
+if (strstr($pay_type, "银联快捷")) {
+  $scan = 'ylkj';
+  $data['payWaySan'] = 'KJZFPC';
+  $form_url = 'http://ngix.xwh98.com/xwhWeb';//pc端提交地址
+  if (_is_mobile()) {
+    $data['payWaySan'] = 'KJZFSJ';
+    $form_url = 'http://ngix.xwh98.com/xwhPhone';//手机端提交地址
+  }
+  $bankname = $pay_type . "->银联快捷在线充值";
+  $payType = $pay_type . "_ylkj";
+} else {
+  $scan = 'wy';
+  $data['payWaySan'] = 'WYPC';
+  $form_url = 'http://ngix.xwh98.com/xwhWeb';//pc端提交地址
+  $bankname = $pay_type . "->网银在线充值";
+  $payType = $pay_type . "_wy";
+}
+
+#新增至资料库，確認訂單有無重複， function在 moneyfunc.php裡(非必要不更动)
+$result_insert = insert_online_order($_REQUEST['S_Name'], $order_no, $mymoney, $bankname, $payType, $top_uid);
+if ($result_insert == -1) {
+  echo "会员信息不存在，无法支付，请重新登录网站进行支付！";
+  exit;
+} else if ($result_insert == -2) {
+  echo "订单号已存在，请返回支付页面重新支付";
+  exit;
+}
+
+#签名排列，可自行组字串或使用http_build_query($array)
+$sign_data = $data;
+ksort($sign_data);
+unset($sign_data['payWaySan']);
+unset($sign_data['schlep']);
+unset($sign_data['sign_']);
+$signtext = stripslashes(json_encode($sign_data)) . $pay_mkey;
+$sign = md5($signtext);
+$data['sign_'] = $sign;
+
+#跳轉方法
+?>
+<html>
+  <head>
+    <title>跳转......</title>
+    <meta http-equiv="content-Type" content="text/html; charset=utf-8" />
+  </head>
+  <body>
+  <form method="get" id="frm1" action="<?php echo $form_url ?>" target="_self">
+     <p>正在为您跳转中，请稍候......</p>
+       <?php foreach ($data as $arr_key => $arr_value) { ?>
+         <input type="hidden" name="<?php echo $arr_key; ?>" value="<?php echo $arr_value; ?>" />
+       <?php 
+    } ?>
+   </form>
+    <script language="javascript">
+      document.getElementById("frm1").submit();
+    </script>
+  </body>
+</html>
+
