c4ed31535d4310aa695b18e1b45bb4a119555fed
alex <alex@egforyou.com>
2018-07-11 14:45:55 +0800

    平安付 修改同步通知

--- project/20180710平安付/pinganful/pinganful_old/return_url.php
+++ project/20180710平安付/pinganful/pinganful_old/return_url.php
@@ -13,70 +13,9 @@ foreach ($_REQUEST as $key => $value) {
 	//write_log($key . "=" . $value);
 }
 
-#设定固定参数
-$order_no = $data['ordernumber']; //订单号
-$mymoney = number_format($data['paymoney'], 2, '.', ''); //订单金额
-$success_msg = $data['orderstatus'];//成功讯息
-$success_code = "1";//文档上的成功讯息
-$sign = $data['sign'];//签名
-$echo_msg = "ok";//回调讯息
 
-#根据订单号读取资料库
-$params = array(':m_order' => $order_no);
-$sql = "select operator from k_money where m_order=:m_order";
-$stmt = $mydata1_db->prepare($sql);
-// $stmt = $mysqlLink->sqlLink("write1")->prepare($sql);//现数据库的连接方式
-$stmt->execute($params);
-$row = $stmt->fetch();
-
-#获取该订单的支付名称
-$pay_type = substr($row['operator'], 0, strripos($row['operator'], "_"));
-$params = array(':pay_type' => $pay_type);
-$sql = "select * from pay_set where pay_type=:pay_type";
-$stmt = $mydata1_db->prepare($sql);
-// $stmt = $mysqlLink->sqlLink("write1")->prepare($sql);//现数据库的连接方式
-$stmt->execute($params);
-$payInfo = $stmt->fetch();
-$pay_mid = $payInfo['mer_id'];
-$pay_mkey = $payInfo['mer_key'];
-$pay_account = $payInfo['mer_account'];
-if ($pay_mid == "" || $pay_mkey == "") {
-	echo "非法提交参数";
-	//write_log("非法提交参数");
-	exit;
-}
-
-#验签方式2
-$signtext = '';
-$signtext .= 'partner='.$data['partner'].'&';
-$signtext .= 'ordernumber='.$data['ordernumber'].'&';
-$signtext .= 'orderstatus='.$data['orderstatus'].'&';
-$signtext .= 'paymoney='.$data['paymoney'].$pay_mkey;
-//write_log("signtext=".$signtext);
-$mysign = md5($signtext);//签名
-//write_log("mysign=".$mysign);
-
-#到账判断
-if ($success_msg == $success_code) {
-	if ($mysign == $sign) {
-		$result_insert = update_online_money($order_no, $mymoney);
-		if ($result_insert == -1) {
-			$message = ("会员信息不存在，无法入账");
-		} else if ($result_insert == 0) {
 			$message = ("支付成功");
-		} else if ($result_insert == -2) {
-			$message = ("数据库操作失败");
-		} else if ($result_insert == 1) {
-			$message = ("支付成功");
-		} else {
-			$message = ("支付失败");
-		}
-	} else {
-		$message = ('签名不正确！');
-	}
-} else {
-	$message = ("交易失败");
-}
+
 ?>
 
 <!-- Html顯示充值資訊 須改變訂單echo變數名稱-->
@@ -96,18 +35,6 @@ if ($success_msg == $success_code) {
 	<table width="98%" border="1" cellspacing="0" cellpadding="3" bordercolordark="#fff" bordercolorlight="#d3d3d3" style="margin: 10px auto;">
     <tr>
 			<td colspan="2" class="tips">处理结果</td>
-		</tr>
-		<tr>
-			<td style="width: 120px; text-align: right;">订单号：</td>
-			<td style="padding-left: 10px;">
-				<label id="lborderno"><?php echo $order_no; ?></label>
-			</td>
-		</tr>
-		<tr>
-			<td style="width: 120px; text-align: right;">充值金额：</td>
-			<td style="padding-left: 10px;">
-				<label id="lbpayamount"><?php echo $mymoney; ?></label>
-			</td>
 		</tr>
 		<tr>
 			<td style="width: 120px; text-align: right;">处理结果：</td>
