84d6bb1ea8bfaac3fafb27ff8785c61126536dc3
fuserion <39684396+fuserion@users.noreply.github.com>
2018-07-17 17:23:34 +0800

    银欣 修改网关地址

---
+++ project/20180717银欣/README.md
@@ -0,0 +1,15 @@
+第三方名称：银欣 
+档案位置：php@60.245.31.1/yinxin 
+ 
+商户号:商户提供的商户号  
+商户密钥KEY：秘钥 后台查看  
+商户交易帐号：无  
+ 
+支持payment：网银,银联快捷,微信扫码,微信wap,京东h5,QQ扫码,QQwap,支付宝扫码,支付宝wap  
+ 
+现有渠道：支付宝WAP  
+测试到账：无  
+  
+修改档案：zfbpost.php,qqpost.php,wxpost.php,post.php,wxjdqqpost.php  
+修改提交网关地址
+ 
---
+++ project/20180717银欣/yinxin/yinxin_old/notify_url.php
@@ -0,0 +1,71 @@
+<? header("content-Type: text/html; charset=UTF-8"); ?>
+<?php
+include_once("../../../database/mysql.config.php");
+include_once("../moneyfunc.php");
+
+$data = array();
+foreach ($_GET as $key => $value) {
+	$data[$key] = $value;
+	//write_log($key."=".$value);
+}
+$params = array(':m_order' => $data['order_no']);
+$sql = "select operator from k_money where m_order=:m_order";
+$stmt = $mydata1_db->prepare($sql);
+$stmt->execute($params);
+$row = $stmt->fetch();
+
+//获取该订单的支付名称
+$pay_type = substr($row['operator'], 0, strripos($row['operator'], "_"));
+
+$params = array(':pay_type' => $pay_type);
+$sql = "select * from pay_set where pay_type=:pay_type";
+$stmt = $mydata1_db->prepare($sql);
+$stmt->execute($params);
+$payInfo = $stmt->fetch();
+$pay_mid = $payInfo['mer_id'];
+$pay_mkey = $payInfo['mer_key'];
+$pay_account = $payInfo['mer_account'];
+
+if ($pay_mid == "" || $pay_mkey == "") {
+	echo "非法提交参数";
+	exit;
+}
+//$ts = time();
+$ts = $data['ts'];
+$signtext='app_id='.$data['app_id'].'is_success='.$data['is_success'].'order_no='.$data['order_no'].'pay_actual_amt='.$data['pay_actual_amt'].$ts.$pay_mkey;
+//write_log("signtext=".$signtext);
+$mysign = md5($signtext);
+//write_log("mysign=".$mysign);
+
+if ($data['is_success'] == "1") {
+  if ( $mysign == $data['sign']) {
+  	$mymoney = number_format($data['pay_actual_amt'], 2, '.', ''); //订单金额
+//write_log("mymoney=".$mymoney);
+		$result_insert = update_online_money($data['order_no'], $mymoney);
+//write_log("result_insert=".$result_insert);
+		if ($result_insert == -1) {
+			echo ("会员信息不存在，无法入账");
+			exit;
+		}else if($result_insert == 0){
+			echo ("ok");
+			exit;
+		}else if($result_insert == -2){
+			echo ("数据库操作失败");
+			exit;
+		}else if($result_insert == 1){
+			echo ("ok");
+			exit;
+		} else {
+			echo ("支付失败");
+			exit;
+		}
+	}else{
+		echo '签名不正确！';
+		exit;
+	}
+}else{
+	echo ("交易失败");
+	exit;
+}
+
+?>
