0000a624ee427728e6451556d9398f08092d6334
fuserion <39684396+fuserion@users.noreply.github.com>
2018-07-18 13:36:21 +0800

    更新RETURN 2种情况的显示

--- project/20180718云酷支付/yunkupay/yunkupay_old/return_url.php
+++ project/20180718云酷支付/yunkupay/yunkupay_old/return_url.php
@@ -1,6 +1,79 @@
 <? header("content-Type: text/html; charset=UTF-8"); ?>
 <?php
-$message = ("支付成功");
+include_once("../../../database/mysql.config.php");
+include_once("../moneyfunc.php");
+
+#接收资料
+#REQUEST方法
+$data = array();
+foreach ($_REQUEST as $key => $value) {
+	$data[$key] = $value;
+	// write_log("return、:".$key."=".$value);
+}
+
+if(!empty($data['fxddh'])){
+	$renodata = 0;
+	#设定固定参数
+	$order_no = $data['fxddh']; //订单号
+	$mymoney = number_format($data['fxfee'], 2, '.', ''); //订单金额
+	$success_msg = $data['fxstatus'];//成功讯息
+	$success_code = "1";//文档上的成功讯息
+	$sign = $data['fxsign'];//签名
+	$echo_msg = "success";//回调讯息
+
+	#根据订单号读取资料库
+	$params = array(':m_order' => $order_no);
+	$sql = "select operator from k_money where m_order=:m_order";
+	$stmt = $mydata1_db->prepare($sql);
+	$stmt->execute($params);
+	$row = $stmt->fetch();
+
+	#获取该订单的支付名称
+	$pay_type = substr($row['operator'], 0, strripos($row['operator'], "_"));
+	$params = array(':pay_type' => $pay_type);
+	$sql = "select * from pay_set where pay_type=:pay_type";
+	$stmt = $mydata1_db->prepare($sql);
+	$stmt->execute($params);
+	$payInfo = $stmt->fetch();
+	$pay_mid = $payInfo['mer_id'];
+	$pay_mkey = $payInfo['mer_key'];
+	$pay_account = $payInfo['mer_account'];
+	if ($pay_mid == "" || $pay_mkey == "") {
+		echo "非法提交参数";
+		exit;
+	}
+
+	#验签方式
+
+	$signtext = $data['fxstatus'].$data['fxid'].$data['fxddh'].$data['fxfee'].$pay_mkey;
+	$mysign = md5($signtext);//签名
+
+
+	#到账判断
+	if ($success_msg == $success_code) {
+	if ( $mysign == $sign) {
+			$result_insert = update_online_money($order_no, $mymoney);
+			if ($result_insert == -1) {
+				$message = ("会员信息不存在，无法入账");
+			}else if($result_insert == 0){
+				$message = ("支付成功");
+			}else if($result_insert == -2){
+				$message = ("数据库操作失败");
+			}else if($result_insert == 1){
+				$message = ("支付成功");
+			} else {
+				$message = ("支付失败");
+			}
+		}else{
+			$message = ('签名不正确！');
+		}
+	}else{
+		$message = ("交易失败");
+	}
+}else{
+	$renodata = 1;
+	$message = ("支付成功");
+}
 ?>
 
 <!-- Html顯示充值資訊 須改變訂單echo變數名稱-->
@@ -21,6 +94,24 @@ $message = ("支付成功");
     <tr>
 			<td colspan="2" class="tips">处理结果</td>
 		</tr>
+		<?php
+		if($renodata == 0){
+		?>
+		<tr>
+			<td style="width: 120px; text-align: right;">订单号：</td>
+			<td style="padding-left: 10px;">
+				<label id="lborderno"><?php echo $order_no; ?></label>
+			</td>
+		</tr>
+		<tr>
+			<td style="width: 120px; text-align: right;">充值金额：</td>
+			<td style="padding-left: 10px;">
+				<label id="lbpayamount"><?php echo $mymoney; ?></label>
+			</td>
+		</tr>
+		<?php
+		}
+		?>
 		<tr>
 			<td style="width: 120px; text-align: right;">处理结果：</td>
 			<td style="padding-left: 10px;">
