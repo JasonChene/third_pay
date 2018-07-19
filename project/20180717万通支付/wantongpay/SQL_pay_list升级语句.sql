a3b720b2a2690bd1123c4b6400601f56163dce96
alex.chang <alex.chang@egforyou.com>
2018-07-19 17:35:18 +0800

    修改第三方_20180717万通支付
    删除多余档案
    修改日志

--- project/20180717万通支付/wantongpay/SQL_pay_list升级语句.sql
+++
@@ -1,9 +0,0 @@
-﻿INSERT INTO pay_list(pay_name,pay_wyUrl,pay_wxUrl,pay_zfbUrl,is_wx,is_wy,is_zfb,
-wy_postUrl,wx_postUrl,zfb_postUrl,
-wy_returnUrl,wx_returnUrl,zfb_returnUrl,
-wy_synUrl,wx_synUrl,zfb_synUrl,is_direct,is_qq,qq_postUrl,qq_returnUrl,qq_synUrl) VALUES
- ('万通支付', '/pay/pay.php', '/pay/wxpay.php', '/pay/zfbpay.php', '1', '1', '1',
- '/pay/wantongpay/post.php', '/pay/wantongpay/wxpost.php', '/pay/wantongpay/zfbpost.php',
- '/pay/wantongpay/return_url.php', '/pay/wantongpay/return_url.php', '/pay/wantongpay/return_url.php',
- '/pay/wantongpay/notify_url.php', '/pay/wantongpay/notify_url.php', '/pay/wantongpay/notify_url.php',
- '1' ,'1','/pay/wantongpay/qqpost.php','/pay/wantongpay/return_url.php','/pay/wantongpay/notify_url.php');
