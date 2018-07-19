b2e6b7536f4dd385c5e15d960e28b401770d3c0d
alex.chang <alex.chang@egforyou.com>
2018-07-19 17:27:58 +0800

    新增第三方_20180717万通支付
    20180717万通支付

---
+++ project/20180717万通支付/wantongpay/common_post.1.php
@@ -0,0 +1,238 @@
+<?php
+header("Content-type:text/html; charset=utf-8");
+// include_once("../../../database/mysql.config.php");//原新数据库的连接方式
+include_once("../../../database/mysql.php");//现数据库的连接方式
+include_once("../moneyfunc.php");
+#预设时间在上海
+date_default_timezone_set('PRC');
+if (function_exists("date_default_timezone_set")) {
+  date_default_timezone_set("Asia/Shanghai");
+}
+
+#function
+function curl_post($url, $data)
+{ #POST访问
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+  curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (compatible; MSIE 5.01; Windows NT 5.0)');
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+  curl_setopt($ch, CURLOPT_AUTOREFERER, 1);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  $tmpInfo = curl_exec($ch);
+  if (curl_errno($ch)) {
+    return curl_error($ch);
+  }
+  return $tmpInfo;
+  curl_close($ch);
+}
+
+function payType_bankname($scan, $pay_type)
+{
+  global $payType, $bankname;
+  if ($scan == "wy") {
+    $payType = $pay_type . "_wy";
+    $bankname = $pay_type . "->网银在线充值";
+  } elseif ($scan == "yl" || $scan == "ylfs") {
+    $payType = $pay_type . "_yl";
+    $bankname = $pay_type . "->银联钱包在线充值";
+  } elseif ($scan == "qq" || $scan == "qqfs") {
+    $payType = $pay_type . "_qq";
+    $bankname = $pay_type . "->QQ钱包在线充值";
+  } elseif ($scan == "wx" || $scan == "wxfs") {
+    $payType = $pay_type . "_wx";
+    $bankname = $pay_type . "->微信在线充值";
+  } elseif ($scan == "zfb" || $scan == "zfbfs") {
+    $payType = $pay_type . "_zfb";
+    $bankname = $pay_type . "->支付宝在线充值";
+  } elseif ($scan == "jd" || $scan == "jdfs") {
+    $payType = $pay_type . "_jd";
+    $bankname = $pay_type . "->京东钱包在线充值";
+  } elseif ($scan == "ylkj") {
+    $payType = $pay_type . "_ylkj";
+    $bankname = $pay_type . "->银联快捷在线充值";
+  } elseif ($scan == "bd" || $scan == "bdfs") {
+    $payType = $pay_type . "_bd";
+    $bankname = $pay_type . "->百度钱包在线充值";
+  } else {
+    echo ('payType_bankname出错啦！');
+    exit;
+  }
+}
+
+function QRcodeUrl($code)
+{ #替换QRcodeUrl中&符号
+  if (strstr($code, "&")) {
+    $code2 = str_replace("&", "aabbcc", $code);//有&换成aabbcc
+  } else {
+    $code2 = $code;
+  }
+  return $code2;
+}
+
+#获取第三方资料(非必要不更动)
+$pay_type = $_REQUEST['pay_type'];
+$params = array(':pay_type' => $pay_type);
+$sql = "select t.pay_name,t.mer_id,t.mer_key,t.mer_account,t.pay_type,t.pay_domain,t1.wy_returnUrl,t1.wx_returnUrl,t1.zfb_returnUrl,t1.wy_synUrl,t1.wx_synUrl,t1.zfb_synUrl from pay_set t left join pay_list t1 on t1.pay_name=t.pay_name where t.pay_type=:pay_type";
+// $stmt = $mydata1_db->prepare($sql);//原新数据库的连接方式
+$stmt = $mysqlLink->sqlLink("write1")->prepare($sql);//现数据库的连接方式
+$stmt->execute($params);
+$row = $stmt->fetch();
+$pay_mid = $row['mer_id'];//商户号
+$pay_mkey = $row['mer_key'];//商戶私钥
+$pay_account = $row['mer_account'];
+$return_url = $row['pay_domain'] . $row['wx_returnUrl'];//return跳转地址
+$merchant_url = $row['pay_domain'] . $row['wx_synUrl'];//notify回传地址
+if ($pay_mid == "" || $pay_mkey == "") {
+  echo "非法提交参数";
+  exit;
+}
+#固定参数设置
+$top_uid = $_REQUEST['top_uid'];
+$order_no = getOrderNo();
+$mymoney = number_format($_REQUEST['MOAmount'], 2, '.', '');
+
+#第三方参数设置
+$data = array(
+  "OOOOO" => $OOOOO, //OOOOO
+
+  "pay_mid" => $pay_mid, //商户号
+  "order_no" => $order_no,//商户流水号/订单号
+  "MOAmount" => number_format($_REQUEST['MOAmount'], 2, '.', ''),//订单金额：精确到小数点后两位
+  "ip" => getClientIp(), //用户端ip
+  "time" => date("Y-m-d H:i:s"), //當下時間：YYYY-MM-DD HH-mm-ss
+  "bank_code" => $_REQUEST['bank_code'], //银行代号
+  "return_url" => $return_url,//return跳转地址
+  "merchant_url" => $merchant_url,//notify回传地址
+);
+
+#变更参数设置
+
+if (_is_mobile()) {
+  $form_url = 'http://a.bzzdp.com/api/createWapOrder';//wap提交地址
+} else {
+  $form_url = 'http://a.bzzdp.com/api/createOrder';//扫码提交地址
+}
+
+if (strstr($pay_type, "京东钱包")) {
+  $scan = 'jd';
+  $data['paytype'] = 'jdOOOOO';
+  if (_is_mobile()) {
+    $data['paytype'] = 'jdwapOOOOO';
+  }
+} elseif (strstr($pay_type, "百度钱包")) {
+  $scan = 'bd';
+  $data['paytype'] = 'bdOOOOO';
+  if (_is_mobile()) {
+    $data['paytype'] = 'bdwapOOOOO';
+  }
+} elseif (strstr($pay_type, "QQ钱包") || strstr($pay_type, "qq钱包")) {
+  $scan = 'qq';
+  $data['paytype'] = 'qqOOOOO';
+  if (_is_mobile()) {
+    $data['paytype'] = 'qqwapOOOOO';
+  };
+} elseif (strstr($pay_type, "支付宝")) {
+  $scan = 'zfb';
+  $data['paytype'] = 'zfbOOOOO';
+  if (_is_mobile()) {
+    $data['paytype'] = 'zfbwapOOOOO';
+  }
+} elseif (strstr($pay_type, "微信反扫")) {
+  $scan = 'wxfs';
+  $data['paytype'] = 'wxOOOOO';
+} else {
+  $scan = 'wx';
+  $data['paytype'] = 'wxOOOOO';
+  if (_is_mobile()) {
+    $data['paytype'] = 'wxwapOOOOO';
+  }
+}
+
+if (strstr($pay_type, "银联钱包")) {
+  $scan = 'yl';
+  $data['paytype'] = 'ylOOOOO';
+} elseif (strstr($pay_type, "银联快捷")) {
+  $scan = 'ylkj';
+  $data['paytype'] = 'ylkjOOOOO';
+} else {
+  $scan = 'wy';
+  $data['paytype'] = 'wyOOOOO';
+}
+payType_bankname($scan, $pay_type);
+
+#新增至资料库，確認訂單有無重複， function在 moneyfunc.php裡(非必要不更动)
+$result_insert = insert_online_order($_REQUEST['S_Name'], $order_no, $mymoney, $bankname, $payType, $top_uid);
+if ($result_insert == -1) {
+  echo "会员信息不存在，无法支付，请重新登录网站进行支付！";
+  exit;
+} else if ($result_insert == -2) {
+  echo "订单号已存在，请返回支付页面重新支付";
+  exit;
+}
+
+#签名排列，可自行组字串或使用http_build_query($array)
+ksort($data);
+$noarr = array('sign');//不加入签名的array key值
+$signtext = '';
+foreach ($data as $arr_key => $arr_val) {
+  if (!in_array($arr_key, $noarr) && (!empty($arr_val) || $arr_val === 0 || $arr_val === '0')) {
+    $signtext .= $arr_key . '=' . $arr_val . '&';
+  }
+}
+$signtext = substr($signtext, 0, -1) . '&key=' . $pay_mkey;
+$sign = md5($signtext);
+$data['sign'] = $sign;
+$data_str = http_build_query($data);
+
+#curl获取响应值
+$res = curl_post($form_url, $data_str);
+$tran = mb_convert_encoding("$res", "UTF-8");
+$row = json_decode($tran, 1);
+
+//打印
+// echo '<pre>';
+// echo ('<br> data = <br>');
+// var_dump($data);
+// echo ('<br> signtext = <br>');
+// echo ($signtext);
+// echo ('<br><br> row = <br>');
+// var_dump($row);
+// echo '</pre>';
+
+// exit;
+
+#跳转
+if ($row['respCode'] != '0000') {
+  echo '错误代码:' . $row['respCode'] . "\n";
+  echo '错误讯息:' . $row['respInfo'] . "\n";
+  exit;
+} else {
+  $qrcodeUrl = $row['qrcodeUrl'];
+  if (_is_mobile()) {
+    $jumpurl = $qrcodeUrl;
+  } else {
+    $jumpurl = '../qrcode/qrcode.php?type=' . $scan . '&code=' . QRcodeUrl($qrcodeUrl);
+  }
+}
+
+#跳轉方法
+?>
+<html>
+  <head>
+    <title>跳转......</title>
+    <meta http-equiv="content-Type" content="text/html; charset=utf-8" />
+  </head>
+  <body>
+    <form method="post" id="frm1" action="<?php echo $jumpurl ?>" target="_self">
+      <p>正在为您跳转中，请稍候......</p>
+    </form>
+    <script language="javascript">
+      document.getElementById("frm1").submit();
+    </script>
+  </body>
+</html>
+
