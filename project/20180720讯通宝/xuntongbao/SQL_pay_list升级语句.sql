05b40720104fd1a70740992f71d561b4e2edf511
alex.chang <alex.chang@egforyou.com>
2018-07-23 09:32:50 +0800

    修改第三方_20180720讯通宝
    移除多余档案

--- project/20180720讯通宝/xuntongbao/SQL_pay_list升级语句.sql
+++
@@ -1,9 +0,0 @@
-﻿INSERT INTO pay_list(pay_name,pay_wyUrl,pay_wxUrl,pay_zfbUrl,is_wx,is_wy,is_zfb,
-wy_postUrl,wx_postUrl,zfb_postUrl,
-wy_returnUrl,wx_returnUrl,zfb_returnUrl,
-wy_synUrl,wx_synUrl,zfb_synUrl,is_direct,is_qq,qq_postUrl,qq_returnUrl,qq_synUrl) VALUES
- ('讯通宝', '/pay/pay.php', '/pay/wxpay.php', '/pay/zfbpay.php', '1', '1', '1',
- '/pay/xuntongbao/post.php', '/pay/xuntongbao/wxpost.php', '/pay/xuntongbao/zfbpost.php',
- '/pay/xuntongbao/return_url.php', '/pay/xuntongbao/return_url.php', '/pay/xuntongbao/return_url.php',
- '/pay/xuntongbao/notify_url.php', '/pay/xuntongbao/notify_url.php', '/pay/xuntongbao/notify_url.php',
- '1' ,'1','/pay/xuntongbao/qqpost.php','/pay/xuntongbao/return_url.php','/pay/xuntongbao/notify_url.php');
