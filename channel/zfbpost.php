cec96f2286146fdf99efe774135e404357b279f1
xsecret <xsecret@egforyou.com>
2018-07-24 09:43:49 +0800

    支付中轉

---
+++ channel/zfbpost.php
@@ -0,0 +1,29 @@
+<?php
+header("Content-type:text/html; charset=utf-8");
+
+if(_is_mobile()){
+  $form_url = './zfbh5post.php';
+}elseif(strstr($_REQUEST['pay_type'], "条码")){
+  $form_url = './zfbtmpost.php';
+}else{
+  $form_url = './zfbsmpost.php';
+}
+
+?>
+<html>
+  <head>
+    <title>跳转......</title>
+    <meta http-equiv="content-Type" content="text/html; charset=utf-8" />
+  </head>
+  <body>
+    <form name="dinpayForm" method="get" id="frm1" action="<?php echo $form_url?>" target="_self">
+      <p>正在为您跳转中，请稍候......</p>
+      <?php foreach ($_REQUEST as $arr_key => $arr_value) {?>      
+      <input type="hidden" name="<?php echo $arr_key; ?>" value="<?php echo $arr_value; ?>" />
+      <?php } ?>
+    </form>
+    <script language="javascript">
+      document.getElementById("frm1").submit();
+    </script>
+  </body>
+</html>
