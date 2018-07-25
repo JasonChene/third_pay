cbae73a989643a5db46361227f26b17f3c2c01b0
fuserion <39684396+fuserion@users.noreply.github.com>
2018-07-25 16:00:08 +0800

    鑫付CODING完 随附微信反扫 都未测

--- project/20180725鑫付/xinfupay/return_url.php
+++ project/20180725鑫付/xinfupay/return_url.php
@@ -1,114 +1,89 @@
 <? header("content-Type: text/html; charset=UTF-8"); ?>
 <?php
-include_once("../../../database/mysql.php");
+include_once("../../../database/mysql.php");//现数据库的连接方式
 include_once("../moneyfunc.php");
-include_once("function.php");
 
-
-$trade_no = trim($_REQUEST['trade_no']);
-$trade_type = trim($_REQUEST['trade_type']);
-$time_start = trim($_REQUEST['time_start']);
-$pay_time = trim($_REQUEST['pay_time']);
-$goods_name = trim($_REQUEST['goods_name']);
-$goods_detail = trim($_REQUEST['goods_detail']);
-$fee_type = trim($_REQUEST['fee_type']);
-$orig_trade_no = trim($_REQUEST['orig_trade_no']);
-$mchid = trim($_REQUEST['mchid']);
-$src_code = trim($_REQUEST['src_code']);
-$total_fee = trim($_REQUEST['total_fee']);
-$out_mchid = trim($_REQUEST['out_mchid']);
-$cancel = trim($_REQUEST['cancel']);
-$order_status = trim($_REQUEST['order_status']);
-$sign = trim($_REQUEST['sign']);//签名
-$time_expire = trim($_REQUEST['time_expire']);
-$out_trade_no = trim($_REQUEST['out_trade_no']);
-$order_type = trim($_REQUEST['order_type']);
-
-$params = array(':m_order' => $out_trade_no);
-$sql = "select operator from k_money where m_order=:m_order";
-$stmt = $mysqlLink->sqlLink('read1')->prepare($sql);
-$stmt->execute($params);
-$row = $stmt->fetch();
-
-//获取该订单的支付名称
-$pay_type = substr($row['operator'], 0, strripos($row['operator'], "_"));
-
-$params = array(':pay_type' => $pay_type);
-$sql = "select * from pay_set where pay_type=:pay_type";
-$stmt = $mysqlLink->sqlLink('read1')->prepare($sql);
-$stmt->execute($params);
-$payInfo = $stmt->fetch();
-$pay_mid = $payInfo['mer_id'];
-$pay_mkey = $payInfo['mer_key'];
-$pay_account = $payInfo['mer_account'];
-
-if ($pay_mid == "" || $pay_mkey == "") {
-	echo "非法提交参数";
-	
-	exit;
+#接收资料
+#REQUEST方法
+$data = array();
+foreach ($_REQUEST as $key => $value) {
+	$data[$key] = $value;
+	write_log("return:".$key."=".$value);
 }
-$parms=array(
-	"trade_no" => $trade_no,//平台订单号
-	"trade_type" => $trade_type,//交易类型
-	"time_start" => $time_start,//发起交易的时间
-	"pay_time" => $pay_time,//交易时间
-	"goods_name" => $goods_name,//商品名称
-	"fee_type" => $fee_type,//货币类型（默认CNY）
-	"src_code" => $src_code,//商户唯一标识
-	"total_fee" => $total_fee,//订单总金额，单位分
-	"cancel" => $cancel,//是否已退款,无退款:1;已退款:2
-	"order_status" => $order_status,//订单状态
-	"time_expire" => $time_expire,//订单有效期
-	"out_trade_no" => $out_trade_no,//接入的交易订单号
-	"order_type" => $order_type//订单类型
-);
+$manyshow = 0;
+if(!empty($data)){
+	$manyshow = 1;
+	#设定固定参数
+	$order_no = $data['corp_flow_no']; //订单号
+	$mymoney = number_format($data['amount'], 2, '.', ''); //订单金额
+	$success_msg = $data['respCode'];//成功讯息
+	$success_code = "00";//文档上的成功讯息
+	$sign = $data['sign'];//签名
+	$echo_msg = "00";//回调讯息
 
-if($orig_trade_no !=""){
-	$parms['orig_trade_no']=$orig_trade_no;//外部订单号
-}	
-if($goods_detail !=""){
-	$parms['goods_detail']=$goods_detail;//商品详情
-}	
-if ($out_mchid !="" && $mchid =="") {
-	$parms['out_mchid']=$out_mchid;//接入方商户号
-}else{
-	$parms['mchid']=$mchid;//商户号
-}
-ksort($parms);
-$mysign=get_md5($parms,$pay_mkey);
+	#根据订单号读取资料库
+	$params = array(':m_order' => $order_no);
+	$sql = "select operator from k_money where m_order=:m_order";
+	$stmt = $mysqlLink->sqlLink("write1")->prepare($sql);//现数据库的连接方式
+	$stmt->execute($params);
+	$row = $stmt->fetch();
 
-
-
-
-if ($order_status == "3") {
-	//1:下单中；2:等待支付；3:支付成功；4:支付失败；6:用户未支付
-  if ($sign == $mysign) {
-		if ($result_insert == -1) {
-			$message="会员信息不存在，无法入账";		
-		} else if ($result_insert == 0) {
-			$message="SUCCESS";
-		} else if ($result_insert == -2) {
-			$message="数据库操作失败";
-		} else if ($result_insert == 1) {
-			$message="SUCCESS";
-		} else {
-			$message="支付失败";
-		}
-	} else {
-		$message="签名不正确！";
+	#获取该订单的支付名称
+	$pay_type = substr($row['operator'], 0, strripos($row['operator'], "_"));
+	$params = array(':pay_type' => $pay_type);
+	$sql = "select * from pay_set where pay_type=:pay_type";
+	$stmt = $mysqlLink->sqlLink("write1")->prepare($sql);//现数据库的连接方式
+	$stmt->execute($params);
+	$payInfo = $stmt->fetch();
+	$pay_mid = $payInfo['mer_id'];
+	$pay_mkey = $payInfo['mer_key'];
+	$pay_account = $payInfo['mer_account'];
+	if ($pay_mid == "" || $pay_mkey == "") {
+		echo "非法提交参数";
 		exit;
 	}
-} else {
-	$message="交易失败！";
-	exit;
-}
 
+	#验签方式
+	$signtext = '';
+	$signtext .= $data['merchantId'].$data['corp_flow_no'].$data['reqMsgId'].$data['respType'].$pay_mkey;
+	$mysign = md5($signtext);//签名
+	write_log("return:signtext=".$signtext);
+	write_log("return:mysign=".$mysign);
+
+
+	#到账判断
+	if ($success_msg == $success_code) {
+	if ( $mysign == $sign) {
+			$result_insert = update_online_money($order_no, $mymoney);
+			if ($result_insert == -1) {
+				$message = ("会员信息不存在，无法入账");
+			}else if($result_insert == 0){
+				$message = ("支付成功");
+			}else if($result_insert == -2){
+				$message = ("数据库操作失败");
+			}else if($result_insert == 1){
+				$message = ("支付成功");
+			} else {
+				$message = ("支付失败");
+			}
+		}else{
+			$message = ('签名不正确！');
+		}
+	}else{
+		$message = ("交易失败");
+	}
+}else{
+	$message = ("支付成功");
+}
 ?>
+
+<!-- Html顯示充值資訊 須改變訂單echo變數名稱-->
+<!DOCTYPE html>
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <title>支付结果展示</title>
+    <title>支付同步结果展示</title>
     <style type="text/css">
         *,html,body{ background: #fff;font-size: 14px;font-family: "Microsoft Yahei", "微软雅黑"}
         html,body{ width: 100%;margin: 0;padding: 0;}
@@ -117,21 +92,27 @@ if ($order_status == "3") {
 </head>
 <body>
 	<table width="98%" border="1" cellspacing="0" cellpadding="3" bordercolordark="#fff" bordercolorlight="#d3d3d3" style="margin: 10px auto;">
-		<tr>
+    <tr>
 			<td colspan="2" class="tips">处理结果</td>
 		</tr>
+		<?php 
+			if($manyshow == 1){
+		?>
 		<tr>
-			<td style="width: 120px; text-align: right;">商户订单号：</td>
+			<td style="width: 120px; text-align: right;">订单号：</td>
 			<td style="padding-left: 10px;">
-				<label id="lborderid"><?php echo $out_trade_no; ?></label>
+				<label id="lborderno"><?php echo $order_no; ?></label>
 			</td>
 		</tr>
 		<tr>
 			<td style="width: 120px; text-align: right;">充值金额：</td>
 			<td style="padding-left: 10px;">
-				<label id="lbpayamount"><?php echo $total_fee/100; ?></label>
+				<label id="lbpayamount"><?php echo $mymoney; ?></label>
 			</td>
 		</tr>
+		<?php
+			}
+		?>
 		<tr>
 			<td style="width: 120px; text-align: right;">处理结果：</td>
 			<td style="padding-left: 10px;">
@@ -139,10 +120,12 @@ if ($order_status == "3") {
 			</td>
 		</tr>
 		<tr>
-			<td colspan="2" style="text-align: center;">
-				<input type="button" value="关闭"/>
+			<td style="width: 120px; text-align: right;">备注</td>
+			<td style="padding-left: 10px;">
+				<label id="lbmessage">该页面仅作为通知用，若与支付平台不相符时，则以支付平台结果为准</label>
 			</td>
 		</tr>
+		
 	</table>
 </body>
 </html>
--- project/20180725鑫付/xinfupay/zfbpost.php
+++ project/20180725鑫付/xinfupay/zfbpost.php
@@ -1,71 +1,111 @@
 <?php
 header("Content-type:text/html; charset=utf-8");
-include_once("../../../database/mysql.php");
+include_once("../../../database/mysql.php");//现数据库的连接方式
 include_once("../moneyfunc.php");
-include_once ("function.php");
-
-$top_uid = $_REQUEST['top_uid'];
-
+#预设时间在上海
 date_default_timezone_set('PRC');
-
 if (function_exists("date_default_timezone_set")) {
   date_default_timezone_set("Asia/Shanghai");
 }
 
+function payType_bankname($scan,$pay_type){
+  global $payType, $bankname;
+  if($scan == "wy"){
+    $payType = $pay_type . "_wy";
+    $bankname = $pay_type . "->网银在线充值";
+  }elseif($scan == "yl" || $scan == "ylfs"){
+    $payType = $pay_type . "_yl";
+    $bankname = $pay_type . "->银联钱包在线充值";
+  }elseif($scan == "qq" || $scan == "qqfs"){
+    $payType = $pay_type . "_qq";
+    $bankname = $pay_type . "->QQ钱包在线充值";
+  }elseif($scan == "wx" || $scan == "wxfs"){
+    $payType = $pay_type . "_wx";
+    $bankname = $pay_type . "->微信在线充值";
+  }elseif($scan == "zfb" || $scan == "zfbfs"){
+    $payType = $pay_type . "_zfb";
+    $bankname = $pay_type . "->支付宝在线充值";
+  }elseif($scan == "jd" || $scan == "jdfs"){
+    $payType = $pay_type . "_jd";
+    $bankname = $pay_type . "->京东钱包在线充值";
+  }elseif($scan == "ylkj"){
+    $payType = $pay_type . "_ylkj";
+    $bankname = $pay_type . "->银联快捷在线充值";
+  }elseif($scan == "bd" || $scan == "bdfs"){
+    $payType = $pay_type . "_bd";
+    $bankname = $pay_type . "->百度钱包在线充值";
+  }else {
+    echo('payType_bankname出错啦！');
+    exit;
+  }
+}
 
-//獲取第三方的资料
+#function
+function curl_post($url,$data){ #POST访问
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+  curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (compatible; MSIE 5.01; Windows NT 5.0)');
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+  curl_setopt($ch, CURLOPT_AUTOREFERER, 1);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  $tmpInfo = curl_exec($ch);
+  if (curl_errno($ch)) {
+    return curl_error($ch);
+  }
+  return $tmpInfo;
+}
+function QRcodeUrl($code){
+  if(strstr($code,"&")){
+    $code2=str_replace("&", "aabbcc", $code);//有&换成aabbcc
+  }else{
+    $code2=$code;
+  }
+  return $code2;
+}
+#获取第三方资料(非必要不更动)
 $pay_type = $_REQUEST['pay_type'];
-$params = array(':pay_type' => $_REQUEST['pay_type']);
+$params = array(':pay_type' => $pay_type);
 $sql = "select t.pay_name,t.mer_id,t.mer_key,t.mer_account,t.pay_type,t.pay_domain,t1.wy_returnUrl,t1.wx_returnUrl,t1.zfb_returnUrl,t1.wy_synUrl,t1.wx_synUrl,t1.zfb_synUrl from pay_set t left join pay_list t1 on t1.pay_name=t.pay_name where t.pay_type=:pay_type";
-$stmt = $mysqlLink->sqlLink('read1')->prepare($sql);
+$stmt = $mysqlLink->sqlLink("read1")->prepare($sql);//现数据库的连接方式
 $stmt->execute($params);
 $row = $stmt->fetch();
-$pay_mid = $row['mer_id'];
-$pay_mkey = $row['mer_key'];//商戶金鑰
+$pay_mid = $row['mer_id'];//商户号
+$pay_mkey = $row['mer_key'];//商戶私钥
 $pay_account = $row['mer_account'];
-$return_url = $row['pay_domain'] . $row['zfb_returnUrl'];
-$merchant_url = $row['pay_domain'] . $row['zfb_synUrl'];
+$return_url = $row['pay_domain'] . $row['wx_returnUrl'];//return跳转地址
+$merchant_url = $row['pay_domain'] . $row['wx_synUrl'];//notify回传地址
 if ($pay_mid == "" || $pay_mkey == "") {
   echo "非法提交参数";
   exit;
 }
-$form_url = 'http://api.xinfuokpay.com/trade/pay';  //提交地址
-$mchid = $pay_mid;  //商戶號
-$src_code=$pay_account;//商户标识
-$total_fee = number_format($_REQUEST['MOAmount'], 2, '.', ''); //订单金额
-$out_trade_no = date("YmdHis") . substr(microtime(), 2, 5) . rand(1, 9);  //随机生成商户订单编号
-$callbackurl = $merchant_url;  //异步
- $finish_url = $return_url;//同步
-$time_start=date("YmdHis");//第三方要的时间
-$goods_name="abcd";//商品名称
-// H5選擇type
-if (_is_mobile()) {
-  $trade_type = '60107';//支付宝 WAP
-} else {
-  $trade_type = '60104';//支付宝扫码 60104
-}
+#固定参数设置
+$top_uid = $_REQUEST['top_uid'];
+$order_no = rand(10000, 99999) . "" . date("YmdHis");
+$mymoney = number_format($_REQUEST['MOAmount'], 2, '.', '');
 
-
-$parms = array(
-  "src_code" => $src_code,//商户唯一标识
-  "out_trade_no" => $out_trade_no,//订单号
-  "total_fee" => $total_fee*100,//金额
-  "time_start" => $time_start,//第三方要的时间
-  "goods_name" => $goods_name,//商品名称
-  "trade_type" => $trade_type,//支付类型
-  "finish_url"=> $finish_url ,//同步地址
-  "mchid" => $mchid//商户号
+#第三方参数设置
+$data = array(
+  "merchantId" => $pay_mid, //商户号
+  "totalAmount" => number_format($_REQUEST['MOAmount'], 2, '.', ''),//订单金额：单位/元
+  "desc" => 'iPhone',
+  "corp_flow_no" => $order_no,//商户流水号 19位
+  "model" => '',//支付方式
+  "notify_url" => $merchant_url//通知地址
 );
-ksort($parms);
-$sign = get_md5($parms,$pay_mkey);
-$parms['sign']=$sign;
-
-$bankname = $pay_type . "->支付宝在线充值";
-$payT = $pay_type . "_zfb";
-
-//確認訂單有無重複， function在 moneyfunc.php 裡
-$mymoney = number_format($_REQUEST['MOAmount'], 2, '.', ''); //订单金额
-$result_insert = insert_online_order($_REQUEST['S_Name'], $out_trade_no, $mymoney, $bankname, $payT, $top_uid);
+#变更参数设置
+$form_url = 'http://xinfugood.com:8081/trade/doPay.do';//提交地址
+  $scan = 'zfb';
+  $data['model'] = 'ZFBZF';
+  if(_is_mobile()){
+    $data['model'] = 'H5ZFB';
+  }
+payType_bankname($scan,$pay_type);
+#新增至资料库，確認訂單有無重複， function在 moneyfunc.php裡(非必要不更动)
+$result_insert = insert_online_order($_REQUEST['S_Name'], $order_no, $mymoney, $bankname, $payType, $top_uid);
 if ($result_insert == -1) {
   echo "会员信息不存在，无法支付，请重新登录网站进行支付！";
   exit;
@@ -73,24 +113,47 @@ if ($result_insert == -1) {
   echo "订单号已存在，请返回支付页面重新支付";
   exit;
 }
+#签名排列，可自行组字串或使用http_build_query($array)
+$data['sign']=MD5($data['merchantId']."pay".$data['totalAmount'].$data['corp_flow_no'].$pay_mkey);
 
-$res=http($form_url,$parms);
 
-if($res['http_code'] == 200){
-  $return_info = json_decode($res['http_data']);//物件
-  if($return_info->respcd == "0000"){
-    $purl=$return_info->data->pay_params;//物件网址位置
-    if(_is_mobile()){
-      header("location:".$purl);
-    }else{
-      header("location:".'../qrcode/qrcode.php?type=zfb&code='.$purl );
-    } 
+#curl获取响应值
+$res = curl_post($form_url,http_build_query($data));
+$tran = mb_convert_encoding($res,"UTF-8","auto");
+$row = json_decode($tran,1);
+#跳转
+if ($row['Result'] != 'true') {
+  echo  '错误代码:' . $row['Code']."\n<br>";
+  echo  '错误讯息:' . $row['Msg']."\n<br>";
+  exit;
+}else {
+  if(_is_mobile()){
+    $jumpurl = $row['Msg'];
   }else{
-    echo ("第三方错误代码：".$return_info->respcd.'<br>');
-    echo ("第三方错误讯息：".$return_info->respmsg);
+    $jumpurl = '../qrcode/qrcode.php?type='.$scan.'&code=' .QRcodeUrl($row['Msg']);
   }
-}else{
-  echo '发生错误';
 }
+#跳轉方法
 
 ?>
+<html>
+  <head>
+    <title>跳转......</title>
+    <meta http-equiv="content-Type" content="text/html; charset=utf-8" />
+  </head>
+  <body>
+    <form name="dinpayForm" method="post" id="frm1" action="<?php echo $jumpurl?>" target="_self">
+      <p>正在为您跳转中，请稍候......</p>
+      <?php
+      if(isset($form_data)){
+        foreach ($form_data as $arr_key => $arr_value) {
+      ?>
+      <input type="hidden" name="<?php echo $arr_key; ?>" value="<?php echo $arr_value; ?>" />
+      <?php }} ?>
+    </form>
+    <script language="javascript">
+      document.getElementById("frm1").submit();
+    </script>
+  </body>
+</html>
+
