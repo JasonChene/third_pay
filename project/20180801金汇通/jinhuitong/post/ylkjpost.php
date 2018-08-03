6cdabe2900d7d30382da70a3695775bc7830aa43
alex <alex@egforyou.com>
2018-08-03 17:55:27 +0800

    修改金汇通

--- project/20180801金汇通/jinhuitong/post/ylkjpost.php
+++ project/20180801金汇通/jinhuitong/post/ylkjpost.php
@@ -2,9 +2,9 @@
 header("Content-type:text/html; charset=utf-8");
 #第三方名稱 : 金汇通
 #支付方式 : ylkj;
-include_once("./addsign.php");
-include_once("../moneyfunc.php");
-include_once("../../../database/mysql.config.php");
+include_once("../addsign.php");
+include_once("../../moneyfunc.php");
+include_once("../../../../database/mysql.php");
 
 
 $S_Name = $_REQUEST['S_Name'];
@@ -13,7 +13,7 @@ $pay_type =$_REQUEST['pay_type'];
 #获取第三方资料(非必要不更动)
 $params = array(':pay_type' => $pay_type);
 $sql = "select t.pay_name,t.mer_id,t.mer_key,t.mer_account,t.pay_type,t.pay_domain,t1.wy_returnUrl,t1.wx_returnUrl,t1.zfb_returnUrl,t1.wy_synUrl,t1.wx_synUrl,t1.zfb_synUrl from pay_set t left join pay_list t1 on t1.pay_name=t.pay_name where t.pay_type=:pay_type";
-$stmt = $mydata1_db->prepare($sql);
+$stmt = $mysqlLink->sqlLink("read1")->prepare($sql);
 $stmt->execute($params);
 $row = $stmt->fetch();
 $pay_mid = $row['mer_id'];
@@ -102,6 +102,7 @@ foreach ($data as $arr_key => $arr_value) {
     $data[$arr_key] = sign_text($arr_value);
   }
 }
+
 $form_data = $data;
 $jumpurl = $form_url;
 ?>
