fd885c4d3ae18b6fb4466f0d7801dc51bc175eec
kewlnes2000 <kewlnes2000@gmail.com>
2018-08-04 10:43:10 +0800

    591支付coding完

---
+++ project/20180804五九一支付/591pay/591pay_old/qqpost.php
@@ -0,0 +1,29 @@
+<?php
+header("Content-type:text/html; charset=utf-8");
+include_once("../moneyfunc.php");
+if(_is_mobile()){
+  $form_url = './qqh5post.php';
+}elseif(strstr($_REQUEST['pay_type'], "反扫")){
+  $form_url = './qqfspost.php';
+}else{
+  $form_url = './qqbspost.php';
+}
+
+?>
+<html>
+  <head>
+    <title>跳转......</title>
+    <meta http-equiv="content-Type" content="text/html; charset=utf-8" />
+  </head>
+  <body>
+    <form name="dinpayForm" method="get" id="frm1" action="<?php echo $form_url?>" target="_self">
+      <p>正在为您跳转中，请稍候......</p>
+      <?php foreach ($_REQUEST as $arr_key => $arr_value) {?>      
+      <input type="hidden" name="<?php echo $arr_key; ?>" value="<?php echo $arr_value; ?>" />
+      <?php } ?>
+    </form>
+    <script language="javascript">
+      document.getElementById("frm1").submit();
+    </script>
+  </body>
+</html>
