06a849211f9c8cb5a8eb02b756a0cc2bb41bc976
alex.chang <alex.chang@egforyou.com>
2018-08-22 13:45:01 +0800

    修改 20180821诚信
    修改网关

---
+++ project/20180821诚信/chengxin/chengxin_old/qqpost.php
@@ -0,0 +1,86 @@
+<?php
+header("Content-type:text/html; charset=utf-8");
+include_once("../../../database/mysql.config.php");
+include_once("../moneyfunc.php");
+
+#function
+
+#获取第三方资料(非必要不更动)
+$pay_type = $_REQUEST['pay_type'];
+$params = array(':pay_type' => $pay_type);
+$sql = "select t.pay_name,t.mer_id,t.mer_key,t.mer_account,t.pay_type,t.pay_domain,t1.wy_returnUrl,t1.wx_returnUrl,t1.zfb_returnUrl,t1.wy_synUrl,t1.wx_synUrl,t1.zfb_synUrl from pay_set t left join pay_list t1 on t1.pay_name=t.pay_name where t.pay_type=:pay_type";
+$stmt = $mydata1_db->prepare($sql);
+$stmt->execute($params);
+$row = $stmt->fetch();
+$pay_mid = $row['mer_id'];//商户号
+$pay_mkey = $row['mer_key'];//商戶私钥
+$pay_account = $row['mer_account'];
+$return_url = $row['pay_domain'] . $row['wx_returnUrl'];//return跳转地址
+$merchant_url = $row['pay_domain'] . $row['wx_synUrl'];//notify回传地址
+if ($pay_mid == "" || $pay_mkey == "") {
+  echo "非法提交参数";
+  exit;
+}
+#固定参数设置
+$top_uid = $_REQUEST['top_uid'];
+$order_no = getOrderNo();
+$mymoney = number_format($_REQUEST['MOAmount'], 2, '.', '');
+
+#第三方参数设置
+$data = array(
+  "bty_appid" => $pay_mid, //商户号
+  "bty_total_fee" => $mymoney,//订单金额：单位/元
+  "bty_type" => '2',//通道 1支付宝，2QQ钱包，3微信
+  "bty_out_trade_no" => $order_no,//订单号
+  "bty_webname" => '诚信支付',//网站名称
+  "bty_subject" => 'iPhone'//商品名称
+);
+#变更参数设置
+$form_url = 'http://www.chengxinzhifu8.com/pay/codepay.php';//提交地址
+$scan = 'qq';
+$bankname = $pay_type . "->QQ钱包在线充值";
+$payType = $pay_type . "_qq";
+
+#新增至资料库，確認訂單有無重複， function在 moneyfunc.php裡(非必要不更动)
+$result_insert = insert_online_order($_REQUEST['S_Name'], $order_no, $mymoney, $bankname, $payType, $top_uid);
+if ($result_insert == -1) {
+  echo "会员信息不存在，无法支付，请重新登录网站进行支付！";
+  exit;
+} else if ($result_insert == -2) {
+  echo "订单号已存在，请返回支付页面重新支付";
+  exit;
+}
+#签名排列，可自行组字串或使用http_build_query($array)
+//$sign = md5($appid.$appkey.$out_trade_no.$total_fee);
+$signtext = $data['bty_appid'] . $pay_mkey . $data['bty_out_trade_no'] . $data['bty_total_fee'];
+
+$sign = md5($signtext);
+$data['sign'] = $sign;
+
+#curl获取响应值
+
+#跳转
+
+#跳轉方法
+
+?>
+<html>
+  <head>
+    <title>跳转......</title>
+    <meta http-equiv="content-Type" content="text/html; charset=utf-8" />
+  </head>
+  <body>
+    <form name="dinpayForm" method="post" id="frm1" action="<?php echo $form_url ?>" target="_self">
+      <p>正在为您跳转中，请稍候......</p>
+      <?php foreach ($data as $arr_key => $arr_value) { ?>      
+      <input type="hidden" name="<?php echo $arr_key; ?>" value="<?php echo $arr_value; ?>" />
+      <?php 
+    } ?>
+    </form>
+    <script language="javascript">
+      document.getElementById("frm1").submit();
+    </script>
+  </body>
+</html>
+
+
