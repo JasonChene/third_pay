3304e7f128805eeec8e422d4422f357feb782029
alex <alex@egforyou.com>
2018-08-27 11:30:02 +0800

    修改首捷网关

--- project/20180718首捷/shoujie/shoujie_old/qqpost.php
+++ project/20180718首捷/shoujie/shoujie_old/qqpost.php
@@ -39,7 +39,7 @@ $data = array(
 
 );
 #变更参数设置
-$form_url = 'http://www.shengxinpay.com/payapi';//提交地址
+$form_url = 'http://www.shoujiepay.com/payapi';//提交地址
 $scan = 'qq';
 $payType = $pay_type . "_qq";
 $bankname = $pay_type . "->QQ钱包在线充值";
