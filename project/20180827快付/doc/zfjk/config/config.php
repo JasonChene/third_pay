e99c0d8129053bafad05b6e767037bebf2ecd50f
alex <alex@egforyou.com>
2018-08-28 11:24:20 +0800

    新增锦绣必付宝 新增快付

---
+++ project/20180827快付/doc/zfjk/config/config.php
@@ -0,0 +1,21 @@
+<?php 
+
+//机构号
+define('INSTCODE', '1000');
+
+//商户号
+define('CERTID', 'ZP10000004');
+//秘钥
+define('PUBLICKEY', 'TLkJcXR0g5mtNMvAtXX8iIzOEgek3HYF');
+//回调地址
+define('BACKURL', 'http://192.168.0.103:94');
+//银联在线支付
+define('UNIONPAY', 'http://39.107.69.36:2502/zpay/02');
+//扫码支付\代付
+define('SCAVENGING', 'http://39.107.69.36:2502/zpay/03');
+//查询订单
+define('QueryOrder', 'http://39.107.69.36:2502/zpay/00');
+
+
+
+?>
---
+++ project/20180827快付/doc/zfjk/init.php
@@ -0,0 +1,6 @@
+<?php 
+
+require(dirname(__FILE__) . '/config/config.php');
+require(dirname(__FILE__) . '/libs/functions.php');
+
+?>
