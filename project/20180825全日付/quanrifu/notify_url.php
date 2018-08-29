c86647a7e0c3e28559d7273d9df36ff81936a6e7
alex <alex@egforyou.com>
2018-08-29 16:20:56 +0800

    全日付 新增md 关掉log

--- project/20180825全日付/quanrifu/notify_url.php
+++ project/20180825全日付/quanrifu/notify_url.php
@@ -3,14 +3,14 @@
 // include_once("../../../database/mysql.config.php");//原数据库的连接方式
 include_once("../../../database/mysql.php");//现数据库的连接方式
 include_once("../moneyfunc.php");
-write_log("notify");
+// write_log("notify");
 
 #接收资料
 #request方法
 $data = array();
 foreach ($_POST as $key => $value) {
 	$data[$key] = $value;
-	write_log($key . "=" . $value);
+	// write_log($key . "=" . $value);
 }
 
 #设定固定参数
@@ -42,7 +42,7 @@ $pay_mkey = $payInfo['mer_key'];
 $pay_account = $payInfo['mer_account'];
 if ($pay_mid == "" || $pay_mkey == "") {
 	echo "非法提交参数";
-	write_log("非法提交参数");
+	// write_log("非法提交参数");
 	exit;
 }
 
@@ -51,14 +51,16 @@ $noarr = array('sign');//不加入签名的array key值
 ksort($data);
 $signtext = "";
 foreach ($data as $arr_key => $arr_val) {
-	if (!in_array($arr_key, $noarr) && (!empty($arr_val) || $arr_val === 0 || $arr_val === '0')) {
+	if ($arr_val == null || $arr_val == 'null') {
+		// write_log($arr_key."=>".$arr_val);
+	}else if (!in_array($arr_key, $noarr) && (!empty($arr_val) || $arr_val === 0 || $arr_val === '0')) {
 		$signtext .= $arr_key . '=' . $arr_val . '&';
 	}
 }
 $signtext = substr($signtext, 0, -1).'&key='.$pay_mkey;//验签字串
-write_log("signtext=".$signtext);
+// write_log("signtext=".$signtext);
 $mysign = strtoupper(md5($signtext));//签名
-write_log("mysign=".$mysign);
+// write_log("mysign=".$mysign);
 
 #到账判断
 if ($success_msg == $success_code) {
@@ -66,28 +68,28 @@ if ($success_msg == $success_code) {
 		$result_insert = update_online_money($order_no, $mymoney);
 		if ($result_insert == -1) {
 			echo ("会员信息不存在，无法入账");
-			write_log("会员信息不存在，无法入账");
+			// write_log("会员信息不存在，无法入账");
 			exit;
 		} else if ($result_insert == 0) {
 			echo ($echo_msg);
-			write_log($echo_msg . 'at 0');
+			// write_log($echo_msg . 'at 0');
 			exit;
 		} else if ($result_insert == -2) {
 			echo ("数据库操作失败");
-			write_log("数据库操作失败");
+			// write_log("数据库操作失败");
 			exit;
 		} else if ($result_insert == 1) {
 			echo ($echo_msg);
-			write_log($echo_msg . 'at 1');
+			// write_log($echo_msg . 'at 1');
 			exit;
 		} else {
 			echo ("支付失败");
-			write_log("支付失败");
+			// write_log("支付失败");
 			exit;
 		}
 	} else {
 		echo ('签名不正确！');
-		write_log("签名不正确！");
+		// write_log("签名不正确！");
 		exit;
 	}
 } else {
