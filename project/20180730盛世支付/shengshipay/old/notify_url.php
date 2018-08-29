cb62ad04e2b4f2a3ce07ac67c2b94cabe63e9ec4
stu640978 <stu640978@gmail.com>
2018-08-29 17:21:50 +0800

    盛世支付修改(重新推)

--- project/20180730盛世支付/shengshipay/old/notify_url.php
+++ project/20180730盛世支付/shengshipay/old/notify_url.php
@@ -1,7 +1,7 @@
 <? header("content-Type: text/html; charset=UTF-8"); ?>
 <?php
-// include_once("../../../database/mysql.config.php");
-include_once("../../../database/mysql.php");//现数据库的连接方式
+include_once("../../../database/mysql.config.php");
+// include_once("../../../database/mysql.php");//现数据库的连接方式
 include_once("../moneyfunc.php");
 // write_log("notify");
 
@@ -9,17 +9,14 @@ include_once("../moneyfunc.php");
 #input方法
 // write_log('input方法');
 $input_data=file_get_contents("php://input");
-$res=json_decode($input_data,1);//json回传资料
+$data=json_decode($input_data,1);//json回传资料
 // write_log($input_data);
-foreach ($_POST as $key => $value) {
-	$data[$key] = $value;
-	// write_log($key."=".$value);
-}
 
 #设定固定参数
-$array  = explode('|',$data['field055']);
+$field055 = substr($data['field055'],0,-1);
+$array = explode("|",$field055);
 $order_no = $array[0]; //订单号
-$mymoney = number_format($array[3], 2, '.', ''); //订单金额
+$mymoney = number_format($array[3]/100, 2, '.', ''); //订单金额
 $success_msg = $array[1];//成功讯息
 $success_code = "00";//文档上的成功讯息
 $sign = $data['field128'];//签名
@@ -27,8 +24,8 @@ $echo_msg = "SUCCESS";//回调讯息
 #根据订单号读取资料库
 $params = array(':m_order' => $order_no);
 $sql = "select operator from k_money where m_order=:m_order";
-// $stmt = $mydata1_db->prepare($sql);
-$stmt = $mysqlLink->sqlLink("write1")->prepare($sql);//现数据库的连接方式
+$stmt = $mydata1_db->prepare($sql);
+// $stmt = $mysqlLink->sqlLink("write1")->prepare($sql);//现数据库的连接方式
 $stmt->execute($params);
 $row = $stmt->fetch();
 
@@ -36,8 +33,8 @@ $row = $stmt->fetch();
 $pay_type = substr($row['operator'], 0, strripos($row['operator'], "_"));
 $params = array(':pay_type' => $pay_type);
 $sql = "select * from pay_set where pay_type=:pay_type";
-// $stmt = $mydata1_db->prepare($sql);
-$stmt = $mysqlLink->sqlLink("write1")->prepare($sql);//现数据库的连接方式
+$stmt = $mydata1_db->prepare($sql);
+// $stmt = $mysqlLink->sqlLink("write1")->prepare($sql);//现数据库的连接方式
 $stmt->execute($params);
 $payInfo = $stmt->fetch();
 $pay_mid = $payInfo['mer_id'];
