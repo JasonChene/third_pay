42ec82205fc56961d13eece493c31f19f98d521b
xsecret@egforyou.com <xsecret@egforyou.com>
2018-08-29 17:31:23 +0800

    365支付2

--- project/20180827-365支付2/365pay2/old/notify_url.php
+++ project/20180827-365支付2/365pay2/old/notify_url.php
@@ -3,16 +3,16 @@
 // include_once("../../../database/mysql.config.php");
 include_once("../../../database/mysql.config.php");
 include_once("../moneyfunc.php");
-write_log("notify");
+// write_log("notify");
 
 #接收资料
 $data = array();
 $input_data=file_get_contents("php://input");
-write_log($input_data);
+// write_log($input_data);
 $res=json_decode($input_data,1);//json回传资料
 foreach ($res as $key => $value) {
 	$data[$key] = $value;
-	write_log($key . "=" . $value);
+	// write_log($key . "=" . $value);
 }
 
 #设定固定参数
@@ -44,14 +44,14 @@ $pay_mkey = $payInfo['mer_key'];
 $pay_account = $payInfo['mer_account'];
 if ($pay_mid == "" || $pay_mkey == "") {
 	echo "非法提交参数";
-	write_log('非法提交参数');
+	// write_log('非法提交参数');
 	exit;
 }
 
 $signtext=$pay_account.$data['OrderNo'].$data['Amount'].$data['Status'].$pay_mkey;
-write_log("signtext=".$signtext);
+// write_log("signtext=".$signtext);
 $mysign = md5($signtext);//签名
-write_log("mysign=".$mysign);
+// write_log("mysign=".$mysign);
 
 
 #到账判断
@@ -60,33 +60,33 @@ if ($success_msg == $success_code) {
 		$result_insert = update_online_money($order_no, $mymoney);
 		if ($result_insert == -1) {
 			echo ("会员信息不存在，无法入账");
-			write_log("会员信息不存在，无法入账");
+			// write_log("会员信息不存在，无法入账");
 			exit;
 		} else if ($result_insert == 0) {
 			echo ($echo_msg);
-			write_log($echo_msg . 'at 0');
+			// write_log($echo_msg . 'at 0');
 			exit;
 		} else if ($result_insert == -2) {
 			echo ("数据库操作失败");
-			write_log("数据库操作失败");
+			// write_log("数据库操作失败");
 			exit;
 		} else if ($result_insert == 1) {
 			echo ($echo_msg);
-			write_log($echo_msg . 'at 1');
+			// write_log($echo_msg . 'at 1');
 			exit;
 		} else {
 			echo ("支付失败");
-			write_log("支付失败");
+			// write_log("支付失败");
 			exit;
 		}
 	} else {
 		echo ('签名不正确！');
-		write_log("签名不正确！");
+		// write_log("签名不正确！");
 		exit;
 	}
 } else {
 	echo ("交易失败");
-	write_log("交易失败");
+	// write_log("交易失败");
 	exit;
 }
 
