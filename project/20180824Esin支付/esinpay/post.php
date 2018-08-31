1a5fea58db78566eab7fbe3529e8f94784ac3aad
stu640978 <stu640978@gmail.com>
2018-08-31 14:27:53 +0800

    修改ESIN支付(将银联快捷Wap导向银联快捷及修改Readme)

--- project/20180824Esin支付/esinpay/post.php
+++ project/20180824Esin支付/esinpay/post.php
@@ -11,7 +11,7 @@ if (strstr($_REQUEST['pay_type'], "银联钱包")){
   }
 }elseif (strstr($_REQUEST['pay_type'], "银联快捷")){
   if(_is_mobile()){
-    $form_url = './ylkjh5post.php';
+    $form_url = './ylkjpost.php';
   }else{
     $form_url = './ylkjpost.php';
   }
