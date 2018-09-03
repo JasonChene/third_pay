5ad55934199cf7c384ebb12d012b73838f47f5c8
alex <alex@egforyou.com>
2018-09-03 14:46:19 +0800

    修改得宝md 修改allpay move

--- all_pay_3.0v/Move/move.js
+++ all_pay_3.0v/Move/move.js
@@ -4,3 +4,20 @@ var path = require('path');
 
 console.log(conf.file_path);
 console.log(conf.move_path);
+
+
+function move(conf) {
+    conf.file_name.forEach(function (e) {
+
+    });
+    fs.rename('./bbb/qqpost.php', './aaa/wxpost.php', function (err) {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log('重命名成功')
+    });
+
+}
+
+move(conf);
