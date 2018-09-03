de5913a1a7e0cb028fd878372a6c56478375b3d6
alex <alex@egforyou.com>
2018-09-03 13:37:11 +0800

    allpay新增move功能 得宝修改银联wap

---
+++ all_pay_3.0v/Move/move.config.js
@@ -0,0 +1,9 @@
+module.exports.setting = {
+    file_path: 'C:/Users/kewlnes2000/Downloads',
+    move_path: 'C:/Users/kewlnes2000/Desktop',
+    message: {
+        err: '搬移档案失败',
+        success: '搬移档案成功'
+    },
+    file_name: ['wypost.php', 'wyh5post.php', 'ylkjpost.php', 'ylkjh5post.php', 'ylbspost.php', 'ylh5post.php', 'ylfspost.php', 'qqbspost.php', 'qqh5post.php', 'qqfspost.php', 'wxbspost.php', 'wxh5post.php', 'wxfspost.php', 'bdbspost.php', 'bdh5post.php', 'bdfspost.php', 'qqbspost.php', 'qqh5post.php', 'qqfspost.php', 'jdbspost.php', 'jdh5post.php', 'jdfspost.php', 'zfbbspost.php', 'zfbh5post.php', 'zfbfspost.php'],
+}
---
+++ all_pay_3.0v/Move/move.js
@@ -0,0 +1,6 @@
+var conf = require('./move.config.js').setting;
+var fs = require('fs');
+var path = require('path');
+
+console.log(conf.file_path);
+console.log(conf.move_path);
