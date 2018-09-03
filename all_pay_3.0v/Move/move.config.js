38e778374d72b4254b525395d4cd3967fa56679d
alex <alex@egforyou.com>
2018-09-03 18:01:45 +0800

    上传allpay move功能

--- all_pay_3.0v/Move/move.config.js
+++ all_pay_3.0v/Move/move.config.js
@@ -1,10 +1,10 @@
 module.exports.setting = {
-    file_path: 'C:/Users/kewlnes2000/Downloads',
-    move_path: 'C:/Users/kewlnes2000/Desktop',
+    file_path: 'D:/job/third_pay2/all_pay_3.0v/Move/aaa/',
+    move_path: 'D:/job/third_pay2/all_pay_3.0v/Move/bbb/',
     message: {
-        err: '搬移档案失败',
-        success: '搬移档案成功'
+        err: '搬移失败档案',
+        success: '搬移成功档案'
     },
     file_name: ['wypost.php', 'wyh5post.php', 'ylkjpost.php', 'ylkjh5post.php', 'ylbspost.php', 'ylh5post.php', 'ylfspost.php', 'qqbspost.php', 'qqh5post.php', 'qqfspost.php', 'wxbspost.php', 'wxh5post.php', 'wxfspost.php', 'bdbspost.php', 'bdh5post.php', 'bdfspost.php', 'qqbspost.php', 'qqh5post.php', 'qqfspost.php', 'jdbspost.php', 'jdh5post.php', 'jdfspost.php', 'zfbbspost.php', 'zfbh5post.php', 'zfbfspost.php'],
-    olf_file_name: ['wypost_old.php', 'wyh5post_old.php', 'ylkjpost_old.php', 'ylkjh5post_old.php', 'ylbspost_old.php', 'ylh5post_old.php', 'ylfspost_old.php', 'qqbspost_old.php', 'qqh5post_old.php', 'qqfspost_old.php', 'wxbspost_old.php', 'wxh5post_old.php', 'wxfspost_old.php', 'bdbspost_old.php', 'bdh5post_old.php', 'bdfspost_old.php', 'qqbspost_old.php', 'qqh5post_old.php', 'qqfspost_old.php', 'jdbspost_old.php', 'jdh5post_old.php', 'jdfspost_old.php', 'zfbbspost_old.php', 'zfbh5post_old.php', 'zfbfspost_old.php'],
+    old_file_name: ['wypost_old.php', 'wyh5post_old.php', 'ylkjpost_old.php', 'ylkjh5post_old.php', 'ylbspost_old.php', 'ylh5post_old.php', 'ylfspost_old.php', 'qqbspost_old.php', 'qqh5post_old.php', 'qqfspost_old.php', 'wxbspost_old.php', 'wxh5post_old.php', 'wxfspost_old.php', 'bdbspost_old.php', 'bdh5post_old.php', 'bdfspost_old.php', 'qqbspost_old.php', 'qqh5post_old.php', 'qqfspost_old.php', 'jdbspost_old.php', 'jdh5post_old.php', 'jdfspost_old.php', 'zfbbspost_old.php', 'zfbh5post_old.php', 'zfbfspost_old.php'],
 }
--- all_pay_3.0v/Move/move.js
+++ all_pay_3.0v/Move/move.js
@@ -1,23 +1,71 @@
 var conf = require('./move.config.js').setting;
 var fs = require('fs');
 var path = require('path');
+var async = require("async");
+var err_file = [],
+    success_file = [];
 
-console.log(conf.file_path);
-console.log(conf.move_path);
-
-
-function move(conf) {
-    conf.file_name.forEach(function (e) {
-
-    });
-    fs.rename('./bbb/qqpost.php', './aaa/wxpost.php', function (err) {
-        if (err) {
-            console.error(err);
-            return;
+function mkdir_old(conf, callback) {
+    //创建old资料夹
+    fs.exists(conf.move_path + 'old', function (exists) {
+        if (exists) {
+            console.log(conf.move_path + 'old' + '目录已存在')
+        } else {
+            fs.mkdir(conf.move_path + 'old', function (err) {
+                if (err)
+                    throw err
+                console.log(conf.move_path + 'old' + '创建目录成功')
+            })
         }
-        console.log('重命名成功')
+        callback(true)
     });
+}
+
+function move(conf, callback) {
+    console.log('开始搬移档案')
+    console.log('原档案位置：' + conf.file_path)
+    console.log('后档案位置：' + conf.move_path)
+    async.forEachOf(conf.file_name, function (file, index, callback) {
+        //搬移新系统档案
+        fs.rename(conf.file_path + file, conf.move_path + file, function (err) {
+            if (err) {
+                err_file.push(file)
+                return
+            }
+            success_file.push(file)
+        });
+
+        //搬移旧系统档案
+        fs.rename(conf.file_path + conf.old_file_name[index], conf.move_path + 'old/' + file, function (err) {
+            if (err) {
+                err_file.push(conf.old_file_name[index])
+                return
+            }
+            success_file.push(conf.old_file_name[index])
+        });
+        callback();
+    }, function (err) {
+        if (err) console.log(err);
+
+        callback(true)
+
+        // configs is now a map of JSON data
+
+    })
+
 
 }
 
-move(conf);
+//执行程式
+mkdir_old(conf, function (done) {
+    if (done) {
+        move(conf, function (e) {
+            if (e) {
+                console.log(conf.message.err)
+                console.log(JSON.stringify(err_file))
+                console.log(conf.message.success)
+                console.log(JSON.stringify(success_file))
+            }
+        });
+    }
+})
