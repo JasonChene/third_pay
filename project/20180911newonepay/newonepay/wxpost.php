0f25b8d1ec46383b9f7de0715ffa120a16344bc7
alex <alex@egforyou.com>
2018-09-11 18:12:13 +0800

    newonepay 修改支持通道

--- project/20180911newonepay/newonepay/wxpost.php
+++ project/20180911newonepay/newonepay/wxpost.php
@@ -21,7 +21,7 @@ if(strstr($_REQUEST['pay_type'], "京东钱包")){
   if(strstr($_REQUEST['pay_type'], "反扫")){
     $form_url = './wxfspost.php';
   }elseif(_is_mobile()){
-    $form_url = './wxh5post.php';
+    $form_url = './wxbspost.php';
   }else{
     $form_url = './wxbspost.php';
   }
