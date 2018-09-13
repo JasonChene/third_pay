2d2750fa885f2898eb1e08ebc25fda3164b81eeb
alex.chang <alex.chang@egforyou.com>
2018-09-13 17:18:11 +0800

    修改 20180912米易支付
    20180912米易支付 修改日志备注
    20180912聚付 新增

---
+++ project/20180912聚付/jifu/card.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+ 	<head>
+ 		<meta charset="utf-8">
+ 		<title></title>
+ 	</head>
+ 	<body>
+		<form action="./post.php" method="get">
+			<?php foreach ($_REQUEST as $arr_key => $arr_value) { ?>
+			<input type="hidden" name="<?php echo $arr_key; ?>" value="<?php echo $arr_value; ?>" />
+			<?php 
+	} ?>
+		<div style="margin-left:2%;color:#f00">输入支付卡号</div><br/>
+		<input type="text" style="width:96%;height:35px;margin-left:2%;" name="cardNo" value=""/><br /><br />
+		<div align="center">
+			<input type="submit" value="送出" style="width:210px; height:50px; border-radius: 15px;background-color:#FE6714; border:0px #FE6714 solid; cursor: pointer;  color:white;  font-size:16px;" />
+		</div>
+		</form>
+ 	</body>
+ </html>
