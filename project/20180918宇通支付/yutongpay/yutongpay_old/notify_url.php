8ab749acb8e3f03ef9d53fde7934d2f441672c71
alex.chang <alex.chang@egforyou.com>
2018-09-18 18:15:44 +0800

    修改 20180918宇通支付
    修改日志
    注解log

--- project/20180918宇通支付/yutongpay/yutongpay_old/notify_url.php
+++ project/20180918宇通支付/yutongpay/yutongpay_old/notify_url.php
@@ -3,47 +3,16 @@
 include_once("../../../database/mysql.config.php");//原数据库的连接方式
 // include_once("../../../database/mysql.php");//现数据库的连接方式
 include_once("../moneyfunc.php");
-#write_log("notify");
-
-
-#############################################
-#request方法
-write_log('request方法');
-foreach ($_REQUEST as $key => $value) {
-	write_log($key . "=" . $value);
-}
-#post方法
-write_log('post方法');
-foreach ($_POST as $key => $value) {
-	write_log($key . "=" . $value);
-}
-#input方法
-write_log('input方法');
-$input_data = file_get_contents("php://input");
-write_log($input_data);
-// $res=json_decode($input_data,1);//json回传资料
-
-// $xml=(array)simplexml_load_string($input_data) or die("Error: Cannot create object");
-// $res=json_decode(json_encode($xml),1);//XML回传资料
-
-// $xml=(array)simplexml_load_string($input_data,'SimpleXMLElement',LIBXML_NOCDATA) or die("Error: Cannot create object");
-// $res=json_decode(json_encode($xml),1);//XMLCDATA回传资料
-
-// foreach ($res as $key => $value) {
-// 	$data[$key] = $value;
-// 	write_log($key."=".$value);
-// }
-###########################################
-
+#//write_log("notify");
 
 #接收资料
 #input方法
 $input_data = file_get_contents("php://input");
-write_log($input_data);
+//write_log($input_data);
 $res = json_decode($input_data, 1);//json回传资料
 foreach ($res['biz_content'] as $key => $value) {
 	$data[$key] = $value;
-	write_log($key . "=" . $value);
+	//write_log($key . "=" . $value);
 }
 
 #设定固定参数
@@ -75,7 +44,7 @@ $pay_mkey = $payInfo['mer_key'];
 $pay_account = $payInfo['mer_account'];
 if ($pay_mid == "" || $pay_mkey == "") {
 	echo "非法提交参数";
-	write_log("非法提交参数");
+	//write_log("非法提交参数");
 	exit;
 }
 
@@ -83,9 +52,9 @@ if ($pay_mid == "" || $pay_mkey == "") {
 ksort($bizContent);
 $bizContent = stripslashes(json_encode($res['biz_content']));
 $signtext = 'biz_content=' . $bizContent . '&key=' . $pay_mkey;
-write_log("signtext=" . $signtext);
+//write_log("signtext=" . $signtext);
 $mysign = strtoupper(md5($signtext));//签名
-write_log("mysign=" . $mysign);
+//write_log("mysign=" . $mysign);
 
 #到账判断
 if ($success_msg == $success_code) {
@@ -93,28 +62,28 @@ if ($success_msg == $success_code) {
 		$result_insert = update_online_money($order_no, $mymoney);
 		if ($result_insert == -1) {
 			echo ("会员信息不存在，无法入账");
-			write_log("会员信息不存在，无法入账");
+			//write_log("会员信息不存在，无法入账");
 			exit;
 		} else if ($result_insert == 0) {
 			echo ($echo_msg);
-			write_log($echo_msg . 'at 0');
+			//write_log($echo_msg . 'at 0');
 			exit;
 		} else if ($result_insert == -2) {
 			echo ("数据库操作失败");
-			write_log("数据库操作失败");
+			//write_log("数据库操作失败");
 			exit;
 		} else if ($result_insert == 1) {
 			echo ($echo_msg);
-			write_log($echo_msg . 'at 1');
+			//write_log($echo_msg . 'at 1');
 			exit;
 		} else {
 			echo ("支付失败");
-			write_log("支付失败");
+			//write_log("支付失败");
 			exit;
 		}
 	} else {
 		echo ('签名不正确！');
-		write_log("签名不正确！");
+		//write_log("签名不正确！");
 		exit;
 	}
 } else {
