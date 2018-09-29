8ad34a153625128ad8f1dd6257edbad56a6789ab
alex <alex@egforyou.com>
2018-09-29 16:03:16 +0800

    上传安唄旧系统

---
+++ project/20180928安呗支付/anbeipay/old/zfbpost.php
@@ -0,0 +1,188 @@
+<?php
+header("Content-type:text/html; charset=utf-8");
+include_once("../../../database/mysql.config.php");
+// include_once("../../../database/mysql.php");//现数据库的连接方式
+include_once("../moneyfunc.php");
+#预设时间在上海
+date_default_timezone_set('PRC');
+if (function_exists("date_default_timezone_set")) {
+  date_default_timezone_set("Asia/Shanghai");
+}
+
+function payType_bankname($scan, $pay_type)
+{
+  global $payType, $bankname;
+  if ($scan == "wy") {
+    $payType = $pay_type . "_wy";
+    $bankname = $pay_type . "->网银在线充值";
+  } elseif ($scan == "yl" || $scan == "ylfs") {
+    $payType = $pay_type . "_yl";
+    $bankname = $pay_type . "->银联钱包在线充值";
+  } elseif ($scan == "qq" || $scan == "qqfs") {
+    $payType = $pay_type . "_qq";
+    $bankname = $pay_type . "->QQ钱包在线充值";
+  } elseif ($scan == "wx" || $scan == "wxfs") {
+    $payType = $pay_type . "_wx";
+    $bankname = $pay_type . "->微信在线充值";
+  } elseif ($scan == "zfb" || $scan == "zfbfs") {
+    $payType = $pay_type . "_zfb";
+    $bankname = $pay_type . "->支付宝在线充值";
+  } elseif ($scan == "jd" || $scan == "jdfs") {
+    $payType = $pay_type . "_jd";
+    $bankname = $pay_type . "->京东钱包在线充值";
+  } elseif ($scan == "ylkj") {
+    $payType = $pay_type . "_ylkj";
+    $bankname = $pay_type . "->银联快捷在线充值";
+  } elseif ($scan == "bd" || $scan == "bdfs") {
+    $payType = $pay_type . "_bd";
+    $bankname = $pay_type . "->百度钱包在线充值";
+  } else {
+    echo ('payType_bankname出错啦！');
+    exit;
+  }
+}
+
+
+#function
+function curl_post($url, $data)
+{ #POST访问
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_POST, true);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+  curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (compatible; MSIE 5.01; Windows NT 5.0)');
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+  curl_setopt($ch, CURLOPT_AUTOREFERER, 1);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  $tmpInfo = curl_exec($ch);
+  if (curl_errno($ch)) {
+    return curl_error($ch);
+  }
+  return $tmpInfo;
+}
+function QRcodeUrl($code)
+{
+  if (strstr($code, "&")) {
+    $code2 = str_replace("&", "aabbcc", $code);//有&换成aabbcc
+  } else {
+    $code2 = $code;
+  }
+  return $code2;
+}
+#获取第三方资料(非必要不更动)
+$S_Name = $_REQUEST['S_Name'];
+$pay_type = $_REQUEST['pay_type'];
+$params = array(':pay_type' => $pay_type);
+$sql = "select t.pay_name,t.mer_id,t.mer_key,t.mer_account,t.pay_type,t.pay_domain,t1.wy_returnUrl,t1.wx_returnUrl,t1.zfb_returnUrl,t1.wy_synUrl,t1.wx_synUrl,t1.zfb_synUrl from pay_set t left join pay_list t1 on t1.pay_name=t.pay_name where t.pay_type=:pay_type";
+$stmt = $mydata1_db->prepare($sql);
+// $stmt = $mysqlLink->sqlLink("read1")->prepare($sql);//现数据库的连接方式
+$stmt->execute($params);
+$row = $stmt->fetch();
+$pay_mid = $row['mer_id'];//商户号
+$pay_mkey = $row['mer_key'];//商戶私钥
+$pay_account = $row['mer_account'];
+$return_url = $row['pay_domain'] . $row['wx_returnUrl'];//return跳转地址
+$merchant_url = $row['pay_domain'] . $row['wx_synUrl'];//notify回传地址
+if ($pay_mid == "" || $pay_mkey == "") {
+  echo "非法提交参数";
+  exit;
+}
+#固定参数设置
+$top_uid = $_REQUEST['top_uid'];
+$order_no = getOrderNo();
+$client_ip = getClientIp();
+$mymoney = number_format($_REQUEST['MOAmount'], 2, '.', '');
+
+#第三方参数设置
+$data = array(
+  "mid" => $pay_mid, //商户号
+  "orderNo" => $order_no,//商户流水号
+  "subject" => 'goods',
+  "body" => 'body',
+  "amount" => number_format($_REQUEST['MOAmount'], 2, '.', ''),//订单金额：单位/元
+  "type" => '',//支付方式
+  "notifyUrl" => $merchant_url,//通知地址
+  "buyerName" => $S_Name,
+  "buyerId" => $S_Name,
+  "payRemark" => 'payRemark',//备注
+  "extNetIp" => $client_ip,
+);
+#变更参数设置
+
+$form_url = 'http://www.paghy.top:8080/pay/CXFServlet/PaySmService?wsdl';
+$scan = 'zfb';
+$data['type'] = 'alipay_srb';
+
+payType_bankname($scan, $pay_type);
+#新增至资料库，確認訂單有無重複， function在 moneyfunc.php裡(非必要不更动)
+$result_insert = insert_online_order($_REQUEST['S_Name'], $order_no, $mymoney, $bankname, $payType, $top_uid);
+if ($result_insert == -1) {
+  echo "会员信息不存在，无法支付，请重新登录网站进行支付！";
+  exit;
+} else if ($result_insert == -2) {
+  echo "订单号已存在，请返回支付页面重新支付";
+  exit;
+}
+#签名排列，可自行组字串或使用http_build_query($array)
+ksort($data);
+$noarr = array('sign','notifyUrl');
+$signtext = '';
+foreach ($data as $arr_key => $arr_val) {
+  if (!in_array($arr_key, $noarr) && (!empty($arr_val) || $arr_val === 0 || $arr_val === '0')) {
+    $signtext .= $arr_key . '=' . $arr_val . '&';
+  }
+}
+$signtext = substr($signtext, 0, -1) . '&key=' . $pay_mkey;
+function des_ecb_encrypt($data, $key){
+  return openssl_encrypt ($data, 'des-ecb', $key);
+}
+$sign = des_ecb_encrypt($pay_mid,$signtext);
+$data['sign'] = $sign; 
+$json_data = json_encode($data);
+
+#soap获取响应值
+try {
+
+  //解决OpenSSL Error问题需要加第二个array参数，具体参考 http://stackoverflow.com/questions/25142227/unable-to-connect-to-wsdl
+  $client = new SoapClient($form_url,
+      array(
+          "stream_context" => stream_context_create(
+              array(
+                  'ssl' => array(
+                      'verify_peer' => false,
+                      'verify_peer_name' => false,
+                  )
+              )
+          )
+      )
+  );
+  // print_r($client->__getFunctions());
+  // print_r($client->__getTypes());
+  // ini_set("soap.wsdl_cache_enabled", "0");
+  $res = $client->pay($json_data);
+  //print_r($res);
+  
+  //json字串转成阵列
+  $row = json_decode($res,1);   
+
+} catch (SOAPFault $e) {
+  print $e;
+}
+
+
+#跳转
+if ($row['code'] != 'SUCCESS') {
+  echo '错误代码:' . $row['code'] . "<br>";
+  echo '错误讯息:' . $row['errCodeDes'] . "<br>";
+  exit;
+} else {
+  #跳轉方法
+  $jumpurl = $row['qrCode'];
+  header('Location:'.$jumpurl);
+  exit;
+}
+
+?>
+
