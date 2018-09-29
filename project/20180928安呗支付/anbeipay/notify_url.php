81758e84d61dbcd40446f6483ae133207d3991e3
alex <alex@egforyou.com>
2018-09-29 16:43:42 +0800

    安唄支付 修改log

--- project/20180928安呗支付/anbeipay/notify_url.php
+++ project/20180928安呗支付/anbeipay/notify_url.php
@@ -3,38 +3,8 @@
 // include_once("../../../database/mysql.config.php");
 include_once("../../../database/mysql.php");//现数据库的连接方式
 include_once("../moneyfunc.php");
-write_log("notify");
+// write_log("notify");
 
-#############################################
-#request方法
-write_log('request方法');
-foreach ($_REQUEST as $key => $value) {
-	// $data[$key] = $value;
-	write_log($key."=".$value);
-}
-#post方法
-write_log('post方法');
-foreach ($_POST as $key => $value) {
-	// $data[$key] = $value;
-	write_log($key."=".$value);
-}
-#input方法
-write_log('input方法');
-$input_data=file_get_contents("php://input");
-write_log($input_data);
-// $res=json_decode($input_data,1);//json回传资料
-
-// $xml=(array)simplexml_load_string($input_data) or die("Error: Cannot create object");
-// $res=json_decode(json_encode($xml),1);//XML回传资料
-
-// $xml=(array)simplexml_load_string($input_data,'SimpleXMLElement',LIBXML_NOCDATA) or die("Error: Cannot create object");
-// $res=json_decode(json_encode($xml),1);//XMLCDATA回传资料
-
-// foreach ($res as $key => $value) {
-// 	$data[$key] = $value;
-// 	write_log($key."=".$value);
-// }
-###########################################
 
 
 #接收资料
@@ -42,7 +12,7 @@ write_log($input_data);
 $data = array();
 foreach ($_POST as $key => $value) {
 	$data[$key] = $value;
-	write_log($key."=".$value);
+	// write_log($key."=".$value);
 }
 
 #设定固定参数
@@ -51,7 +21,7 @@ $mymoney = number_format($data['amount'], 2, '.', ''); //订单金额
 $success_msg = $data['code'];//成功讯息
 $success_code = "1";//文档上的成功讯息
 $sign = $data['sign'];//签名
-$echo_msg = "";//回调讯息
+$echo_msg = "success";//回调讯息
 
 #根据订单号读取资料库
 $params = array(':m_order' => $order_no);
@@ -74,7 +44,7 @@ $pay_mkey = $payInfo['mer_key'];
 $pay_account = $payInfo['mer_account'];
 if ($pay_mid == "" || $pay_mkey == "") {
 	echo "非法提交参数";
-	write_log("非法提交参数");
+	// write_log("非法提交参数");
 	exit;
 }
 
@@ -88,12 +58,12 @@ foreach ($data as $arr_key => $arr_val) {
 	}
 }
 $signtext = substr($signtext, 0, -1) . '&key=' . $pay_mkey;
-write_log("signtext=".$signtext);
+// write_log("signtext=".$signtext);
 function des_ecb_encrypt($data, $key){
 	return openssl_encrypt ($data, 'des-ecb', $key);
   }
 $mysign = des_ecb_encrypt($pay_mid,$signtext);
-write_log("mysign=".$mysign);
+// write_log("mysign=".$mysign);
 
 #到账判断
 if ($success_msg == $success_code) {
@@ -101,33 +71,33 @@ if ($success_msg == $success_code) {
 		$result_insert = update_online_money($order_no, $mymoney);
 		if ($result_insert == -1) {
 			echo ("会员信息不存在，无法入账");
-			write_log("会员信息不存在，无法入账");
+			// write_log("会员信息不存在，无法入账");
 			exit;
 		}else if($result_insert == 0){
 			echo ($echo_msg);
-			write_log($echo_msg.'at 0');
+			// write_log($echo_msg.'at 0');
 			exit;
 		}else if($result_insert == -2){
 			echo ("数据库操作失败");
-			write_log("数据库操作失败");
+			// write_log("数据库操作失败");
 			exit;
 		}else if($result_insert == 1){
 			echo ($echo_msg);
-			write_log($echo_msg.'at 1');
+			// write_log($echo_msg.'at 1');
 			exit;
 		} else {
 			echo ("支付失败");
-			write_log("支付失败");
+			// write_log("支付失败");
 			exit;
 		}
 	}else{
 		echo ('签名不正确！');
-		write_log("签名不正确！");
+		// write_log("签名不正确！");
 		exit;
 	}
 }else{
 	echo ("交易失败");
-	write_log("交易失败");
+	// write_log("交易失败");
 	exit;
 }
 
