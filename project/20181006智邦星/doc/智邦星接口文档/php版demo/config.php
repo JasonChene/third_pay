617b4472d2334be2c0e5e93054d47514551cf29e
xsecret@egforyou.com <xsecret@egforyou.com>
2018-10-07 16:39:04 +0800

    嘉亿，智邦星，新合發

---
+++ project/20181006智邦星/doc/智邦星接口文档/php版demo/config.php
@@ -0,0 +1,11 @@
+<?php
+	$payUrl="http://www.zbxpay.com/pay";//支付提交地址
+	$queryUrl="http://www.zbxpay.com/query";//查询提交地址
+	
+	$merchant = "866001";//商户号
+	$key = "53021e2cae854d2e8fd32e2e89e5ec1c";//商户密钥
+	
+	$notifyUrl="http://demo.zbxzf.com/notifyUrl.php";//异步回调
+	$returnUrl="http://demo.zbxzf.com/returnUrl.php";//同步回调
+	
+?>
