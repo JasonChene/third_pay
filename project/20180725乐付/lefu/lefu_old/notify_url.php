428a29cd07d07a3a26825d38e9444bd20672702c
stu640978 <stu640978@gmail.com>
2018-10-16 22:39:55 +0800

    修改乐付(qqpost,wxpost,wxqqjdpost,zfbbpost,post(修改网关地址,跳转地址),修改Readme,新增log档)

--- project/20180725乐付/lefu/lefu_old/notify_url.php
+++ project/20180725乐付/lefu/lefu_old/notify_url.php
@@ -2,16 +2,16 @@
 <?php
 include_once("../../../database/mysql.config.php");//原数据库的连接方式
 include_once("../moneyfunc.php");
-#//write_log("notify");
+// write_log("notify");
 
 #接收资料
 #input方法
 $input_data = file_get_contents("php://input");
-//write_log($input_data);
+// write_log($input_data);
 $res = json_decode($input_data, 1);//json回传资料
 foreach ($res as $key => $value) {
 	$data[$key] = $value;
-	//write_log($key . "=" . $value);
+	// write_log($key . "=" . $value);
 }
 
 #设定固定参数
@@ -41,7 +41,7 @@ $pay_mkey = $payInfo['mer_key'];
 $pay_account = $payInfo['mer_account'];
 if ($pay_mid == "" || $pay_mkey == "") {
 	echo "非法提交参数";
-	//write_log("非法提交参数");
+	// write_log("非法提交参数");
 	exit;
 }
 
@@ -55,9 +55,9 @@ foreach ($data as $arr_key => $arr_val) {
 	}
 }
 $signtext = substr($signtext, 0, -1) . '&key=' . $pay_mkey;//验签字串
-//write_log("signtext=" . $signtext);
+// write_log("signtext=" . $signtext);
 $mysign = strtoupper(md5($signtext));//签名
-//write_log("mysign=" . $mysign);
+// write_log("mysign=" . $mysign);
 
 #到账判断
 if ($success_msg == $success_code) {
@@ -65,28 +65,28 @@ if ($success_msg == $success_code) {
 		$result_insert = update_online_money($order_no, $mymoney);
 		if ($result_insert == -1) {
 			echo ("会员信息不存在，无法入账");
-			//write_log("会员信息不存在，无法入账");
+			// write_log("会员信息不存在，无法入账");
 			exit;
 		} else if ($result_insert == 0) {
 			echo ($echo_msg);
-			//write_log($echo_msg . 'at 0');
+			// write_log($echo_msg . 'at 0');
 			exit;
 		} else if ($result_insert == -2) {
 			echo ("数据库操作失败");
-			//write_log("数据库操作失败");
+			// write_log("数据库操作失败");
 			exit;
 		} else if ($result_insert == 1) {
 			echo ($echo_msg);
-			//write_log($echo_msg . 'at 1');
+			// write_log($echo_msg . 'at 1');
 			exit;
 		} else {
 			echo ("支付失败");
-			//write_log("支付失败");
+			// write_log("支付失败");
 			exit;
 		}
 	} else {
 		echo ('签名不正确！');
-		//write_log("签名不正确！");
+		// write_log("签名不正确！");
 		exit;
 	}
 } else {
--- project/20180725乐付/lefu/wxpost.php
+++ project/20180725乐付/lefu/wxpost.php
@@ -109,16 +109,13 @@ $data = array(
 );
 
 #变更参数设置
-$form_url = 'http://114.115.136.13/pay/unifiedorder';//返回json请求URL
+$form_url = 'http://114.116.71.237/pay/unifiedorder';//返回json请求URL
 if (strstr($pay_type, "京东钱包")) {
   $scan = 'jd';
   $data['payType'] = 'H5-jd';
 } else {
   $scan = 'wx';
-  $data['payType'] = 'QRCode-wx';
-  if (_is_mobile()) {
-    $data['payType'] = 'H5-wx';
-  }
+  $data['payType'] = 'H5-wx';
 }
 payType_bankname($scan, $pay_type);
 
@@ -167,7 +164,7 @@ if ($jumptype == '1') {
     }
   }
 } else {
-  $jumpurl = 'http://114.115.136.13/pay/redirect/unifiedorder';//页面跳转请求URL
+  $jumpurl = 'http://114.116.71.237/pay/redirect/unifiedorder';//页面跳转请求URL
 }
 
 #跳轉方法
