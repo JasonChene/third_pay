62d023f2f0a3c506364171aa8eae83b215622e9a
xsecret@egforyou.com <xsecret@egforyou.com>
2018-10-19 16:20:04 +0800

    蜻蜓付

---
+++ project/20181019蜻蜓付/doc/demo/src/main/java/org/xxpay/demo/api/BaseSdk.java
@@ -0,0 +1,36 @@
+package org.xxpay.demo.api;
+
+import org.xxpay.demo.util.HttpClient;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * @author: dingzhiwei
+ * @date: 2018/4/16
+ * @description:
+ */
+public class BaseSdk {
+
+    /**
+     * 发起HTTP/HTTPS请求(method=POST)
+     * @param url
+     * @return
+     */
+    public static String call4Post(String url) {
+        try {
+            URL url1 = new URL(url);
+            if("https".equals(url1.getProtocol())) {
+                return HttpClient.callHttpsPost(url);
+            }else if("http".equals(url1.getProtocol())) {
+                return HttpClient.callHttpPost(url);
+            }else {
+                return "";
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+}
