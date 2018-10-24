93041d2b604230e03d89f59add3f87e3b84062db
alex.chang <alex.chang@egforyou.com>
2018-10-24 11:25:15 +0800

    修改 20181019 9付宝
    20181019 9付宝 完成

--- project/20181019 9付宝/9fubao/9fubao_old/wxpost.php
+++ project/20181019 9付宝/9fubao/9fubao_old/wxpost.php
@@ -1,28 +1,28 @@
 <?php
 header("Content-type:text/html; charset=utf-8");
 include_once("../moneyfunc.php");
-if(strstr($_REQUEST['pay_type'], "京东钱包")){
-  if(strstr($_REQUEST['pay_type'], "反扫")){
+if (strstr($_REQUEST['pay_type'], "京东钱包")) {
+  if (strstr($_REQUEST['pay_type'], "反扫")) {
     $form_url = './jdfspost.php';
-  }elseif(_is_mobile()){
-    $form_url = './jdh5post.php';
-  }else{
+  // }elseif(_is_mobile()){
+  // $form_url = './jdh5post.php';
+  } else {
     $form_url = './jdbspost.php';
   }
-}elseif(strstr($_REQUEST['pay_type'], "百度钱包")){
-  if(strstr($_REQUEST['pay_type'], "反扫")){
+} elseif (strstr($_REQUEST['pay_type'], "百度钱包")) {
+  if (strstr($_REQUEST['pay_type'], "反扫")) {
     $form_url = './bdfspost.php';
-  }elseif(_is_mobile()){
+  } elseif (_is_mobile()) {
     $form_url = './bdh5post.php';
-  }else{
+  } else {
     $form_url = './bdbspost.php';
   }
-}else{
-  if(strstr($_REQUEST['pay_type'], "反扫")){
+} else {
+  if (strstr($_REQUEST['pay_type'], "反扫")) {
     $form_url = './wxfspost.php';
-  }elseif(_is_mobile()){
+  } elseif (_is_mobile()) {
     $form_url = './wxh5post.php';
-  }else{
+  } else {
     $form_url = './wxbspost.php';
   }
 }
@@ -34,11 +34,12 @@ if(strstr($_REQUEST['pay_type'], "京东钱包")){
     <meta http-equiv="content-Type" content="text/html; charset=utf-8" />
   </head>
   <body>
-    <form name="dinpayForm" method="get" id="frm1" action="<?php echo $form_url?>" target="_self">
+    <form name="dinpayForm" method="get" id="frm1" action="<?php echo $form_url ?>" target="_self">
       <p>正在为您跳转中，请稍候......</p>
-      <?php foreach ($_REQUEST as $arr_key => $arr_value) {?>      
+      <?php foreach ($_REQUEST as $arr_key => $arr_value) { ?>      
       <input type="hidden" name="<?php echo $arr_key; ?>" value="<?php echo $arr_value; ?>" />
-      <?php } ?>
+      <?php 
+    } ?>
     </form>
     <script language="javascript">
       document.getElementById("frm1").submit();
