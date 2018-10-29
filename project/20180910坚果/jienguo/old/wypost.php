36a4db173f5e44e244d099bf2dd29a85506071f6
alex.chang <alex.chang@egforyou.com>
2018-10-29 19:23:00 +0800

    修改 20180910坚果
    第三方修改网关

--- project/20180910坚果/jienguo/old/wypost.php
+++ project/20180910坚果/jienguo/old/wypost.php
@@ -9,7 +9,7 @@ include_once("../../../database/mysql.config.php");
 
 $S_Name = $_REQUEST['S_Name'];
 $top_uid = $_REQUEST['top_uid'];
-$pay_type =$_REQUEST['pay_type'];
+$pay_type = $_REQUEST['pay_type'];
 #获取第三方资料(非必要不更动)
 $params = array(':pay_type' => $pay_type);
 $sql = "select t.pay_name,t.mer_id,t.mer_key,t.mer_account,t.pay_type,t.pay_domain,t1.wy_returnUrl,t1.wx_returnUrl,t1.zfb_returnUrl,t1.wy_synUrl,t1.wx_synUrl,t1.zfb_synUrl from pay_set t left join pay_list t1 on t1.pay_name=t.pay_name where t.pay_type=:pay_type";
@@ -28,7 +28,7 @@ if ($pay_mid == "" || $pay_mkey == "") {
 
 
 #固定参数设置
-$form_url = 'http://www.ztmmk.com.cn/Pay_Index.html';
+$form_url = 'http://www.jieyibaopay.com/Pay_Index.html';
 $bank_code = $_REQUEST['bank_code'];
 $order_no = getOrderNo();
 $notify_url = $merchant_url;
@@ -42,44 +42,44 @@ $mymoney = number_format($_REQUEST['MOAmount'], 2, '.', '');
 $MOAmount = number_format($_REQUEST['MOAmount'], 0, '.', '');
 #第三方传值参数设置
 $data = array(
-"pay_memberid" => $pay_mid,
-"pay_orderid" => $order_no,
-"pay_amount" => $MOAmount,
-"pay_notifyurl" => $notify_url,
-"pay_applydate" => $order_time,
-"pay_bankcode" => '907',
-"pay_callbackurl" => $return_url,
-"pay_productname" => 'productname',
-"pay_md5sign" => array(
-"str_arr" => array(
-"pay_amount" => $MOAmount,
-"pay_applydate" => $order_time,
-"pay_bankcode" => "907",
-"pay_callbackurl" => $return_url,
-"pay_memberid" => $pay_mid,
-"pay_notifyurl" => $notify_url,
-"pay_orderid" => $order_no,
-),
-"mid_conn" => "=",
-"last_conn" => "&",
-"encrypt" => array(
-"0" => "MD5",
-"1" => "upper",
-),
-"key_str" => "&key=",
-"key" => $pr_key,
-"havekey" => "1",
-),
+  "pay_memberid" => $pay_mid,
+  "pay_orderid" => $order_no,
+  "pay_amount" => $MOAmount,
+  "pay_notifyurl" => $notify_url,
+  "pay_applydate" => $order_time,
+  "pay_bankcode" => '907',
+  "pay_callbackurl" => $return_url,
+  "pay_productname" => 'productname',
+  "pay_md5sign" => array(
+    "str_arr" => array(
+      "pay_amount" => $MOAmount,
+      "pay_applydate" => $order_time,
+      "pay_bankcode" => "907",
+      "pay_callbackurl" => $return_url,
+      "pay_memberid" => $pay_mid,
+      "pay_notifyurl" => $notify_url,
+      "pay_orderid" => $order_no,
+    ),
+    "mid_conn" => "=",
+    "last_conn" => "&",
+    "encrypt" => array(
+      "0" => "MD5",
+      "1" => "upper",
+    ),
+    "key_str" => "&key=",
+    "key" => $pr_key,
+    "havekey" => "1",
+  ),
 );
 #变更参数设定
-$payType = $pay_type."_wy";
-$bankname = $pay_type."->网银在线充值";
+$payType = $pay_type . "_wy";
+$bankname = $pay_type . "->网银在线充值";
 #新增至资料库，確認訂單有無重複， function在 moneyfunc.php裡(非必要不更动)
-$result_insert = insert_online_order($S_Name , $order_no , $mymoney,$bankname,$payType,$top_uid);
-if ($result_insert == -1){
+$result_insert = insert_online_order($S_Name, $order_no, $mymoney, $bankname, $payType, $top_uid);
+if ($result_insert == -1) {
   echo "会员信息不存在，无法支付，请重新登录网站进行支付！";
   exit;
-} else if ($result_insert == -2){
+} else if ($result_insert == -2) {
   echo "订单号已存在，请返回支付页面重新支付";
   exit;
 }
@@ -100,14 +100,16 @@ $jumpurl = $form_url;
       <meta http-equiv="content-Type" content="text/html; charset=utf-8" />
   </head>
   <body>
-      <form name="dinpayForm" method="post" id="frm1" action="<?php echo $jumpurl?>" target="_self">
+      <form name="dinpayForm" method="post" id="frm1" action="<?php echo $jumpurl ?>" target="_self">
           <p>正在为您跳转中，请稍候......</p>
           <?php
-          if(isset($form_data)){
-              foreach ($data as $arr_key => $arr_value) {
-          ?>
+          if (isset($form_data)) {
+            foreach ($data as $arr_key => $arr_value) {
+              ?>
               <input type="hidden" name="<?php echo $arr_key; ?>" value="<?php echo $arr_value; ?>" />
-          <?php }} ?>
+          <?php 
+        }
+      } ?>
       </form>
       <script language="javascript">
           document.getElementById("frm1").submit();
