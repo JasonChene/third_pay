64ec15d4e950fd6cd0f41f2668bfc7ae5f66fb24
alex <alex@egforyou.com>
2018-10-31 14:41:07 +0800

    新增付唄 修改收盈宝md

--- project/20181030快线支付/kuaixianpay/old/notify_url.php
+++ project/20181030快线支付/kuaixianpay/old/notify_url.php
@@ -3,38 +3,8 @@
 include_once("../../../database/mysql.config.php");
 // include_once("../../../database/mysql.php");//现数据库的连接方式
 include_once("../moneyfunc.php");
-write_log("notify");
+// write_log("notify");
 
-#############################################
-#request方法
-write_log('request方法');
-foreach ($_REQUEST as $key => $value) {
-	// $data[$key] = $value;
-	write_log($key."=".$value);
-}
-#post方法
-write_log('post方法');
-foreach ($_POST as $key => $value) {
-	// $data[$key] = $value;
-	write_log($key."=".$value);
-}
-#input方法
-write_log('input方法');
-$input_data=file_get_contents("php://input");
-write_log($input_data);
-// $res=json_decode($input_data,1);//json回传资料
-
-// $xml=(array)simplexml_load_string($input_data) or die("Error: Cannot create object");
-// $res=json_decode(json_encode($xml),1);//XML回传资料
-
-// $xml=(array)simplexml_load_string($input_data,'SimpleXMLElement',LIBXML_NOCDATA) or die("Error: Cannot create object");
-// $res=json_decode(json_encode($xml),1);//XMLCDATA回传资料
-
-// foreach ($res as $key => $value) {
-// 	$data[$key] = $value;
-// 	write_log($key."=".$value);
-// }
-###########################################
 
 
 #接收资料
@@ -42,7 +12,7 @@ write_log($input_data);
 $data = array();
 foreach ($_POST as $key => $value) {
 	$data[$key] = $value;
-	write_log($key . "=" . $value);
+	// write_log($key . "=" . $value);
 }
 
 #设定固定参数
@@ -79,7 +49,7 @@ if ($pay_mid == "" || $pay_mkey == "") {
 
 #验签方式
 ksort($data);
-$noarr = array('sign','paytype');
+$noarr = array('sign');
 $signtext = '';
 foreach ($data as $arr_key => $arr_val) {
   if (!in_array($arr_key, $noarr) && (!empty($arr_val) || $arr_val === 0 || $arr_val === '0')) {
@@ -89,41 +59,42 @@ foreach ($data as $arr_key => $arr_val) {
 
 
 $signtext = substr($signtext, 0, -1) . '&' . $pay_mkey;
+// write_log($signtext);
 $mysign = md5($signtext);
-
+// write_log($mysign);
 #到账判断
 if ($success_msg == $success_code) {
 	if ($mysign == $sign) {
 		$result_insert = update_online_money($order_no, $mymoney);
 		if ($result_insert == -1) {
 			echo ("会员信息不存在，无法入账");
-			write_log("会员信息不存在，无法入账");
+			// write_log("会员信息不存在，无法入账");
 			exit;
 		} else if ($result_insert == 0) {
 			echo ($echo_msg);
-			write_log($echo_msg . 'at 0');
+			// write_log($echo_msg . 'at 0');
 			exit;
 		} else if ($result_insert == -2) {
 			echo ("数据库操作失败");
-			write_log("数据库操作失败");
+			// write_log("数据库操作失败");
 			exit;
 		} else if ($result_insert == 1) {
 			echo ($echo_msg);
-			write_log($echo_msg . 'at 1');
+			// write_log($echo_msg . 'at 1');
 			exit;
 		} else {
 			echo ("支付失败");
-			write_log("支付失败");
+			// write_log("支付失败");
 			exit;
 		}
 	} else {
 		echo ('签名不正确！');
-		write_log("签名不正确！");
+		// write_log("签名不正确！");
 		exit;
 	}
 } else {
 	echo ("交易失败");
-	write_log("交易失败");
+	// write_log("交易失败");
 	exit;
 }
 
---
+++ project/20181031付唄/fubei/old/zfbh5post.php
@@ -0,0 +1,114 @@
+<?php
+header("Content-type:text/html; charset=utf-8");
+#第三方名稱 : 付唄
+#支付方式 : zfb;
+include_once("./addsign.php");
+include_once("../moneyfunc.php");
+include_once("../../../database/mysql.config.php");
+
+
+$S_Name = $_REQUEST['S_Name'];
+$top_uid = $_REQUEST['top_uid'];
+$pay_type =$_REQUEST['pay_type'];
+#获取第三方资料(非必要不更动)
+$params = array(':pay_type' => $pay_type);
+$sql = "select t.pay_name,t.mer_id,t.mer_key,t.mer_account,t.pay_type,t.pay_domain,t1.wy_returnUrl,t1.wx_returnUrl,t1.zfb_returnUrl,t1.wy_synUrl,t1.wx_synUrl,t1.zfb_synUrl from pay_set t left join pay_list t1 on t1.pay_name=t.pay_name where t.pay_type=:pay_type";
+$stmt = $mydata1_db->prepare($sql);
+$stmt->execute($params);
+$row = $stmt->fetch();
+$pay_mid = $row['mer_id'];
+$pay_mkey = $row['mer_key'];
+$pay_account = $row['mer_account'];
+$return_url = trim($row['pay_domain'] . $row['wx_returnUrl']);//同步
+$merchant_url = trim($row['pay_domain'] . $row['wx_synUrl']);//异步
+if ($pay_mid == "" || $pay_mkey == "") {
+  echo "非法提交参数";
+  exit;
+}
+
+
+#固定参数设置
+$form_url = 'http://www.pd55.top/apisubmit';
+$bank_code = $_REQUEST['bank_code'];
+$order_no = getOrderNo();
+$notify_url = $merchant_url;
+$client_ip = getClientIp();
+$pr_key = $pay_mkey;//私钥
+$pu_key = $pay_account;//公钥
+$order_time = date("YmdHis");
+
+
+$mymoney = number_format($_REQUEST['MOAmount'], 2, '.', '');
+$MOAmount = number_format($_REQUEST['MOAmount'], 2, '.', '');
+#第三方传值参数设置
+$data = array(
+"customerid" => $pay_mid,
+"sdorderno" => $order_no,
+"total_fee" => $MOAmount,
+"notifyurl" => $notify_url,
+"version" => '1.0',
+"returnurl" => $return_url,
+"paytype" => 'alipaywap',
+"get_code" => '0',
+"sign" => array(
+"str_arr" => array(
+"version" => "1.0",
+"customerid" => $pay_mid,
+"total_fee" => $MOAmount,
+"sdorderno" => $order_no,
+"notifyurl" => $notify_url,
+"returnurl" => $return_url,
+),
+"mid_conn" => "=",
+"last_conn" => "&",
+"encrypt" => array(
+"0" => "MD5",
+),
+"key_str" => "&",
+"key" => $pr_key,
+"havekey" => "1",
+),
+);
+#变更参数设定
+$payType = $pay_type."_zfb";
+$bankname = $pay_type."->支付宝在线充值";
+#新增至资料库，確認訂單有無重複， function在 moneyfunc.php裡(非必要不更动)
+$result_insert = insert_online_order($S_Name , $order_no , $mymoney,$bankname,$payType,$top_uid);
+if ($result_insert == -1){
+  echo "会员信息不存在，无法支付，请重新登录网站进行支付！";
+  exit;
+} else if ($result_insert == -2){
+  echo "订单号已存在，请返回支付页面重新支付";
+  exit;
+}
+
+
+#签名排列，可自行组字串或使用http_build_query($array)
+foreach ($data as $arr_key => $arr_value) {
+  if (is_array($arr_value)) {
+    $data[$arr_key] = sign_text($arr_value);
+  }
+}
+$form_data = $data;
+$jumpurl = $form_url;
+?>
+<html>
+  <head>
+      <title>跳转......</title>
+      <meta http-equiv="content-Type" content="text/html; charset=utf-8" />
+  </head>
+  <body>
+      <form name="dinpayForm" method="post" id="frm1" action="<?php echo $jumpurl?>" target="_self">
+          <p>正在为您跳转中，请稍候......</p>
+          <?php
+          if(isset($form_data)){
+              foreach ($data as $arr_key => $arr_value) {
+          ?>
+              <input type="hidden" name="<?php echo $arr_key; ?>" value="<?php echo $arr_value; ?>" />
+          <?php }} ?>
+      </form>
+      <script language="javascript">
+          document.getElementById("frm1").submit();
+      </script>
+   </body>
+</html>
