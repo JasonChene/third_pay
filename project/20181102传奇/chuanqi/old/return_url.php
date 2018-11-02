f59fd4ca645cca8a55bb924d5ec1d046f3463aaa
alex.chang <alex.chang@egforyou.com>
2018-11-02 14:37:11 +0800

    修改 20181102传奇 修改 return

--- project/20181102传奇/chuanqi/old/return_url.php
+++ project/20181102传奇/chuanqi/old/return_url.php
@@ -1,77 +1,3 @@
-<? header("content-Type: text/html; charset=UTF-8"); ?>
-<?php
-include_once("../../../database/mysql.config.php");//原数据库的连接方式
-// include_once("../../../database/mysql.php");//现数据库的连接方式
-include_once("../moneyfunc.php");
-
-#接收资料
-#接收资料
-#input方法
-$input_data = file_get_contents("php://input");
-$res = json_decode($input_data, 1);//json回传资料
-foreach ($res as $key => $value) {
-	$data[$key] = $value;
-}
-
-#设定固定参数
-$order_no = $data['orderNo']; //订单号
-$mymoney = number_format($data['orderAmt'], 2, '.', ''); //订单金额
-$success_msg = $data['payStatus'];//成功讯息
-$success_code = "S";//文档上的成功讯息
-$sign = $data['sign'];//签名
-$echo_msg = "success";//回调讯息
-
-#根据订单号读取资料库
-$params = array(':m_order' => $order_no);
-$sql = "select operator from k_money where m_order=:m_order";
-$stmt = $mydata1_db->prepare($sql);//原数据库的连接方式
-// $stmt = $mysqlLink->sqlLink("read1")->prepare($sql);//现数据库的连接方式
-$stmt->execute($params);
-$row = $stmt->fetch();
-
-#获取该订单的支付名称
-$pay_type = substr($row['operator'], 0, strripos($row['operator'], "_"));
-$params = array(':pay_type' => $pay_type);
-$sql = "select * from pay_set where pay_type=:pay_type";
-$stmt = $mydata1_db->prepare($sql);//原数据库的连接方式
-// $stmt = $mysqlLink->sqlLink("read1")->prepare($sql);//现数据库的连接方式
-$stmt->execute($params);
-$payInfo = $stmt->fetch();
-$pay_mid = $payInfo['mer_id'];
-$pay_mkey = $payInfo['mer_key'];
-$pay_account = $payInfo['mer_account'];
-if ($pay_mid == "" || $pay_mkey == "") {
-	echo "非法提交参数";
-	exit;
-}
-
-#验签方式
-$signtext = "merId=" . $data['merId'] . "&orderAmt=" . $data['orderAmt'] . "&orderNo=" . $data['orderNo'] . "&payDate=" . $data['payDate'] . "&payNo=" . $data['payNo'] . "&payStatus=" . $data['payStatus'] . "&payTime=" . $data['payTime'] . "&realAmt=" . $data['realAmt'] . "&remark1=" . $data['remark1'] . "&remark2=" . $data['remark2'] . "&version=" . $data['version'] . "&key=" . $pay_mkey;//验签字串
-$mysign = md5($signtext);//签名
-
-#到账判断
-if ($success_msg == $success_code) {
-	if ($mysign == $sign) {
-		$result_insert = update_online_money($order_no, $mymoney);
-		if ($result_insert == -1) {
-			$message = ("会员信息不存在，无法入账");
-		} else if ($result_insert == 0) {
-			$message = ("支付成功");
-		} else if ($result_insert == -2) {
-			$message = ("数据库操作失败");
-		} else if ($result_insert == 1) {
-			$message = ("支付成功");
-		} else {
-			$message = ("支付失败");
-		}
-	} else {
-		$message = ('签名不正确！');
-	}
-} else {
-	$message = ("交易失败");
-}
-?>
-
 <!-- Html顯示充值資訊 須改變訂單echo變數名稱-->
 <!DOCTYPE html>
 
@@ -91,21 +17,9 @@ if ($success_msg == $success_code) {
 			<td colspan="2" class="tips">处理结果</td>
 		</tr>
 		<tr>
-			<td style="width: 120px; text-align: right;">订单号</td>
-			<td style="padding-left: 10px;">
-				<label id="lborderno"><?php echo $order_no; ?></label>
-			</td>
-		</tr>
-		<tr>
-			<td style="width: 120px; text-align: right;">充值金额</td>
-			<td style="padding-left: 10px;">
-				<label id="lbpayamount"><?php echo $mymoney; ?></label>
-			</td>
-		</tr>
-		<tr>
 			<td style="width: 120px; text-align: right;">处理结果</td>
 			<td style="padding-left: 10px;">
-				<label id="lbmessage"><?php echo $message; ?></label>
+				<label id="lbmessage">支付成功</label>
 			</td>
 		</tr>
 		<tr>
